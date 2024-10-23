503d88bdb330b80ac467404bd1193996ab3b17e7
atamayev <75034340+atamayev@users.noreply.github.com>
2024-10-23 14:55:30 -0400

    Centering text

--- src/components/blue-dot.tsx
+++ src/components/blue-dot.tsx
@@ -1,15 +1,13 @@
 export default function BlueDot() {
-	const dotStyle: React.CSSProperties = {
-		backgroundColor: "rgb(0,61,165)"
-	}
-
 	return (
-		<>
+		<div className="flex flex-col items-center">
 			<div
 				className="w-48 h-48 rounded-full border-2"
-				style={dotStyle}
+				style={{ backgroundColor: "rgb(0,61,165)" }}
 			/>
-			Blue Dot Robots
-		</>
+			<div className="text-3xl font-semibold mb-4 text-zinc-800 dark:text-zinc-50 text-center">
+				Blue Dot Robots
+			</div>
+		</div>
 	)
 }
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,36 +1,15 @@
-import { useCallback, useEffect, useState } from "react"
 import BlueDot from "../components/blue-dot"
 import PageHelmet from "../components/helmet/page-helmet"
 
 export default function Home() {
-	const [minHeight, setMinHeight] = useState("100vh")
-
-	const handleResize = useCallback(() => {
-		const footerHeight = document.getElementById("footer")?.offsetHeight || 0
-		const headerHeight = document.getElementById("header")?.offsetHeight || 0
-		const viewportHeight = window.innerHeight
-		const margin = 32 * 4 // 32 is the tailwind mt-32, multiplied by 4 to convert to pixels
-		const contentMinHeight = viewportHeight - headerHeight - footerHeight - margin
-		setMinHeight(`${contentMinHeight}px`)
-	}, [])
-
-	useEffect(() => {
-		window.addEventListener("resize", handleResize)
-		handleResize()
-
-		return () => window.removeEventListener("resize", handleResize)
-	}, [handleResize])
-
 	return (
 		<>
 			<PageHelmet pageTitle="/" />
-			<div style={{ minHeight }}>
+			<div>
 				<div className="flex justify-center w-full mt-32">
 					<div className="w-full max-w-3xl">
 						<div className="flex flex-col items-center">
-							<div className="text-3xl font-semibold mb-4 text-zinc-800 dark:text-zinc-50 text-center">
-								<BlueDot />
-							</div>
+							<BlueDot />
 						</div>
 					</div>
 				</div>
