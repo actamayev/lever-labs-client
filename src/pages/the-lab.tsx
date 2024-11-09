06b86d03ec7e84fb28f2505700f24d98b0d1b7f1
atamayev <75034340+actamayev@users.noreply.github.com>
2024-11-09 11:36:15 -0500

    Move to sandbox

--- src/pages/the-lab.tsx
+++ src/pages/the-lab.tsx
@@ -1,7 +1,6 @@
 import { observer } from "mobx-react"
 import { useAuthContext } from "../contexts/auth-context"
 import PageHelmet from "../components/helmet/page-helmet"
-import BlocklyComponent from "../components/blockly-component"
 import ShowAuthToNullUser from "../components/show-auth-to-null-user"
 
 function TheLab() {
@@ -22,8 +21,6 @@ function TheLab() {
 			<div className="text-black dark:text-white text-3xl">
 				The Lab
 			</div>
-			<BlocklyComponent />
-			<h2>Generated Code:</h2>
 		</>
 	)
 }
--- src/pages/the-sandbox.tsx
+++ src/pages/the-sandbox.tsx
@@ -1,6 +1,7 @@
 import { observer } from "mobx-react"
 import { useAuthContext } from "../contexts/auth-context"
 import PageHelmet from "../components/helmet/page-helmet"
+import BlocklyComponent from "../components/blockly-component"
 import ShowAuthToNullUser from "../components/show-auth-to-null-user"
 
 function TheSandbox() {
@@ -21,6 +22,7 @@ function TheSandbox() {
 			<div className="text-black dark:text-white text-3xl">
 				The Sandbox
 			</div>
+			{/* <BlocklyComponent /> */}
 		</>
 	)
 }
