6abe85844a04b743c152629a1ce69c45845d734c
Ariel Tamayev <actamayev@gmail.com>
2024-11-11 16:42:25 -0500

    Blocks (#16)
    * Add auto connect (#10)
    
    * Add auto connect
    
    * Add id
    
    * cleanup
    
    ---------
    
    Co-authored-by: atamayev <75034340+atamayev@users.noreply.github.com>
    
    * Build sandbox (#13)
    
    * Add new pages
    
    * Conditionally enter pip naem
    
    * Cleanup
    
    * Update add-pip-modal.tsx
    
    * Pip uuid
    
    * Re-arrange code
    
    * Cleanup
    
    * Blockly JS
    
    * Move to sandbox
    
    ---------
    
    Co-authored-by: atamayev <75034340+actamayev@users.noreply.github.com>
    
    * Blocks (#15)
    
    * Make Pip blocks
    
    * Type assertions
    
    * Create blocks
    
    * Split up create all blocks
    
    * Styles
    
    * Cleanup
    
    ---------
    
    Co-authored-by: atamayev <75034340+actamayev@users.noreply.github.com>
    
    * Delete custom-blocks.ts
    
    ---------
    
    Co-authored-by: actamayev <75034340+actamayev@users.noreply.github.com>
    Co-authored-by: atamayev <75034340+atamayev@users.noreply.github.com>

---
+++ src/utils/blockly/custom-blocks/sensor-blocks.ts
@@ -0,0 +1,73 @@
+import * as Blockly from "blockly"
+import { Order } from "blockly/javascript"
+import { sensorsCategory } from "../toolbox-config"
+import { SENSORS_BLOCK_TYPES, IMUSensorType, LeftRightSensorType, IRSensorType, SENSOR_TYPES, SensorsBlockNames } from "../block-types"
+
+export const sensorsBlocks: Record<SensorsBlockNames, CustomBlock> = {
+	[SENSORS_BLOCK_TYPES.IMU_READ]: {
+		definition: {
+			type: SENSORS_BLOCK_TYPES.IMU_READ,
+			message0: "Read IMU %1",
+			args0: [
+				{
+					type: "field_dropdown",
+					name: "IMU_VALUE",
+					options: Object.entries(SENSOR_TYPES.IMU).map(([key, value]) =>
+                        [key.toLowerCase(), value] as [string, string]
+					)
+				}
+			],
+			output: "Number",
+			colour: sensorsCategory.colour,
+			tooltip: "Read value from 9-axis IMU sensor"
+		},
+		generator: (block: Blockly.Block): [string, number] => {
+			const value = block.getFieldValue("IMU_VALUE") as IMUSensorType
+			return [`IMU.read("${value}")`, Order.FUNCTION_CALL]
+		}
+	},
+	[SENSORS_BLOCK_TYPES.TOF_READ]: {
+		definition: {
+			type: SENSORS_BLOCK_TYPES.TOF_READ,
+			message0: "Read distance from %1 ToF sensor",
+			args0: [
+				{
+					type: "field_dropdown",
+					name: "SENSOR",
+					options: Object.entries(SENSOR_TYPES.LEFTRIGHT).map(([key, value]) =>
+                        [key.toLowerCase(), value] as [string, string]
+					)
+				}
+			],
+			output: "Number",
+			colour: sensorsCategory.colour,
+			tooltip: "Read distance in mm from Time of Flight sensor"
+		},
+		generator: (block: Blockly.Block): [string, number] => {
+			const sensor = block.getFieldValue("SENSOR") as LeftRightSensorType
+			return [`ToF.read("${sensor}")`, Order.FUNCTION_CALL]
+		}
+	},
+	[SENSORS_BLOCK_TYPES.IR_READ]: {
+		definition: {
+			type: SENSORS_BLOCK_TYPES.IR_READ,
+			message0: "Read IR sensor %1",
+			args0: [
+				{
+					type: "field_dropdown",
+					name: "SENSOR",
+					options: Object.entries(SENSOR_TYPES.IR).map(([key, value]) =>
+                        [key.toLowerCase(), value] as [string, string]
+					)
+				}
+			],
+			output: "Number",
+			colour: sensorsCategory.colour,
+			tooltip: "Read value from infrared sensor"
+		},
+		generator: (block: Blockly.Block): [string, number] => {
+			const sensor = block.getFieldValue("SENSOR") as IRSensorType
+			return [`IR.read("${sensor}")`, Order.FUNCTION_CALL]
+		}
+	}
+}
