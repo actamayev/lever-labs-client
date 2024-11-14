2a5957f607f014e1c9fc1d45b0cd90cb8313ac11
atamayev <75034340+actamayev@users.noreply.github.com>
2024-11-14 12:38:36 -0500

    Update blockly-component.tsx

--- src/components/blockly-component.tsx
+++ src/components/blockly-component.tsx
@@ -91,11 +91,11 @@ export default function BlocklyComponent() {
 					{blocklyState.cppCode}
 				</pre>
 			</div>
-			<Button
+			{/* <Button
 				title="Send code to Pip"
 				onClick={sendCodeToCppCallback}
 				disabled={_.isEmpty(blocklyState.cppCode)}
-			/>
+			/> */}
 		</div>
 	)
 }
