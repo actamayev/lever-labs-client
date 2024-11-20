3948f46d1de308216d9b8279e05f2cf6d7df103a
Ariel Tamayev <actamayev@gmail.com>
2024-11-20 17:42:48 -0500

    Sandbox (#27)
    * New led colors codde
    
    * Disable button when sending code
    
    * Update blockly-component.tsx
    
    ---------
    
    Co-authored-by: atamayev <75034340+actamayev@users.noreply.github.com>

--- src/hooks/pip/send-cpp-to-pip.ts
+++ src/hooks/pip/send-cpp-to-pip.ts
@@ -1,5 +1,6 @@
 import _ from "lodash"
 import { useCallback } from "react"
+import { usePipContext } from "../../contexts/pip-context"
 import { isNonSuccessResponse } from "../../utils/type-checks"
 import { useNotificationsContext } from "../../contexts/notifications-context"
 import { useApiClientContext } from "../../contexts/blue-dot-api-client-context"
@@ -8,6 +9,7 @@ export default function useSendCppToPip(): (
 	pipUUID: PipUUID,
 	cppCode: string
 ) => Promise<void> {
+	const pipClass = usePipContext()
 	const blueDotApiClient = useApiClientContext()
 	const notificationsClass = useNotificationsContext()
 
@@ -16,18 +18,23 @@ export default function useSendCppToPip(): (
 		cppCode: string
 	) => {
 		try {
+			if (pipClass.isSendingCppToPip === true) return
+			pipClass.setIsSendingCppToPip(true)
+
 			const connectToPipResponse = await blueDotApiClient.pipDataService.sendCppToPip(pipUUID, cppCode)
 
 			if (!_.isEqual(connectToPipResponse.status, 200) || isNonSuccessResponse(connectToPipResponse.data)) {
 				throw new Error("Connect to Pip failed")
 			}
-			// pipClass.updatePipConnectionStatus({ pipUUID: pipUUID, newConnectionStatus: "connected" })
 			notificationsClass.setPositiveNotification("Code sent to Pip")
 		} catch (error) {
 			console.error(error)
 			notificationsClass.setNegativeNotification(
 				"Unable to upload code to Pip at this time. Please reload page and try again."
 			)
+		} finally {
+			pipClass.setIsSendingCppToPip(false)
 		}
-	}, [blueDotApiClient.pipDataService, notificationsClass])
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [blueDotApiClient.pipDataService, notificationsClass, pipClass.isSendingCppToPip])
 }
