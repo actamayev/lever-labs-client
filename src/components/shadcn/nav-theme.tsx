f6b894aa20743aa7bbf74c76025878c53fd361e1
atamayev <75034340+actamayev@users.noreply.github.com>
2024-11-25 17:25:55 -0500

    Update nav-theme.tsx

--- src/components/shadcn/nav-theme.tsx
+++ src/components/shadcn/nav-theme.tsx
@@ -11,7 +11,7 @@ function NavTheme() {
 	return (
 		<SidebarMenu>
 			<SidebarMenuItem>
-				<Button variant="outline" size="icon" onClick={setDefaultSiteTheme}>
+				<Button variant= "ghost" size="icon" onClick={setDefaultSiteTheme}>
 					<Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
 					<Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
 				</Button>
