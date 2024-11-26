860453ee1079beed88bcab249f351b10b9de92ba
atamayev <75034340+actamayev@users.noreply.github.com>
2024-11-26 00:59:06 -0500

    Update sandbox-sidebar.tsx

--- src/components/shadcn/sidebar/secondary/sandbox-sidebar.tsx
+++ src/components/shadcn/sidebar/secondary/sandbox-sidebar.tsx
@@ -5,6 +5,7 @@ import {
 } from "@/components/shadcn/ui/sidebar"
 
 export default function SandboxSidebar() {
+	// TODO: Add a map of the user's past projects here. search should filter, there should be an add project button
 	return (
 		<Sidebar collapsible="none" className="hidden flex-1 md:flex">
 			<SidebarHeader className="gap-3.5 border-b p-4">
