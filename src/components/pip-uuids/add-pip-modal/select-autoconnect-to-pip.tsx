d5e6782c6c5e2e902002ac9e015a66df0bb1956c
atamayev <75034340+actamayev@users.noreply.github.com>
2024-11-27 10:53:24 -0500

    Update select-autoconnect-to-pip.tsx

--- src/components/pip-uuids/add-pip-modal/select-autoconnect-to-pip.tsx
+++ src/components/pip-uuids/add-pip-modal/select-autoconnect-to-pip.tsx
@@ -8,8 +8,10 @@ export default function SelectAutoreconnectToPip({ control } : { control: Contro
 			control={control}
 			name="shouldAutoConnect"
 			render={({ field }) => (
-				<FormItem className="mt-3 grid gap-2">
-					<FormLabel>Auto-connect?</FormLabel>
+				<FormItem className="flex items-center justify-between space-x-2">
+					<FormLabel className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+						Auto-connect?
+					</FormLabel>
 					<FormControl>
 						<Slider
 							id="pip-auto-connect-slider"
