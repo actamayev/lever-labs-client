351677331cab450a06ef1a4e8bb807753fd68cef
atamayev <75034340+actamayev@users.noreply.github.com>
2024-11-27 17:43:33 -0500

    Fix inset header

--- src/components/layouts/internal-pages-layout.tsx
+++ src/components/layouts/internal-pages-layout.tsx
@@ -9,15 +9,17 @@ import { AppSidebar } from "@/components/shadcn/sidebar/app-sidebar"
 
 export default function InternalPagesLayout({ children } : { children: React.ReactNode }) {
 	return (
-		<SidebarProvider style={ { "--sidebar-width": "400px" } as React.CSSProperties }>
+		<SidebarProvider style={{ "--sidebar-width": "400px" } as React.CSSProperties}>
 			<AppSidebar />
 			<SidebarInset>
-				<header className="sticky top-0 flex shrink-0 items-center gap-2 border-b bg-background p-4 z-40">
+				<header className="fixed w-full top-0 flex shrink-0 items-center gap-2 border-b bg-background p-4 z-40">
 					<SidebarTrigger className="-ml-1 size-10"/>
 					<Separator orientation="vertical" className="mr-2 h-4" />
 					<PipUuids />
 				</header>
-				{ children }
+				<div className="pt-16">
+					{children}
+				</div>
 			</SidebarInset>
 		</SidebarProvider>
 	)
