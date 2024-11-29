17d43eff5c5efaf064bf3e960e77dc1451b52222
atamayev <75034340+actamayev@users.noreply.github.com>
2024-11-29 13:40:22 -0500

    Update pip-render.tsx

--- src/components/home/pip-render.tsx
+++ src/components/home/pip-render.tsx
@@ -1,13 +1,16 @@
+import BlurFade from "../shadcn/ui/blur-fade"
 import TextRevealByWord from "../shadcn/ui/text-reveal"
 
 export default function PipRender() {
 	return (
 		<div className="flex flex-col items-center justify-center w-full">
-			<img
-				src="pip-render-11-28.png"
-				alt="Product visualization"
-				className="rounded-lg object-cover w-full max-w-[700px]"
-			/>
+			<BlurFade delay={0.3 * 3} inView>
+				<img
+					src="pip-render-11-28.png"
+					alt="Product visualization"
+					className="rounded-lg object-cover w-full max-w-[700px]"
+				/>
+			</BlurFade>
 			<div className="my-24">
 				<TextRevealByWord
 					text="Designed to make learning robotics fun and seamless."
