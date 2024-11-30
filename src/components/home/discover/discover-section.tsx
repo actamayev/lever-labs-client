7e4e98db06d24345d4920b7d80f11bce2379010b
atamayev <75034340+actamayev@users.noreply.github.com>
2024-11-30 10:28:10 -0500

    Go to dotted page

--- src/components/home/discover/discover-section.tsx
+++ src/components/home/discover/discover-section.tsx
@@ -1,5 +1,6 @@
 import BoldSpanText from "../bold-span-text"
 import { LabAndSandboxBento } from "./lab-and-sandbox-bento"
+import DottedUnderlineText from "../../dotted-underline-text"
 
 export default function DiscoverSection() {
 	return (
@@ -13,7 +14,15 @@ export default function DiscoverSection() {
 			<p className="my-8 text-zinc-600 dark:text-zinc-400 text-center text-3xl">
 				Pip pairs with Blue Dot, our dynamic web platform, purpose-built to make your robotics education journey fun and powerful.
 				Start with beginner-friendly coding blocks, then graduate to real-world programming.
-				Whether you&apos;re solving guided challenges in the Lab, or exploring Sandbox mode,&nbsp;
+				Whether you&apos;re solving guided challenges in the&nbsp;
+				<DottedUnderlineText linkTo="/lab">
+					Lab
+				</DottedUnderlineText>
+				, or exploring the&nbsp;
+				<DottedUnderlineText linkTo="/sandbox">
+					Sandbox
+				</DottedUnderlineText>
+				,&nbsp;
 				<BoldSpanText>
 					every moment with Pip is hands-on, rewarding, and
 					uniquely yours.
