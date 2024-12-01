03d26bb42970256fd26faa8adf88e270011150dc
atamayev <75034340+actamayev@users.noreply.github.com>
2024-12-01 18:58:00 -0500

    On logout, set theme to light

--- src/components/auth/google/google-sign-in.tsx
+++ src/components/auth/google/google-sign-in.tsx
@@ -1,11 +1,8 @@
-import { observer } from "mobx-react"
 import { GoogleLogin } from "@react-oauth/google"
-import useDefaultSiteTheme from "../../../hooks/memos/default-site-theme"
 import useGoogleAuthCallback from "../../../hooks/auth/google/google-auth-callback"
 
-function GoogleSignIn({ whereToNavigate }: { whereToNavigate: PageNames }) {
+export default function GoogleSignIn({ whereToNavigate }: { whereToNavigate: PageNames }) {
 	const googleAuthCallback = useGoogleAuthCallback(whereToNavigate)
-	const defaultSiteTheme = useDefaultSiteTheme()
 
 	return (
 		<div className="flex justify-center">
@@ -14,12 +11,9 @@ function GoogleSignIn({ whereToNavigate }: { whereToNavigate: PageNames }) {
 				onError={() => console.error("Login Failed")}
 				shape="rectangular"
 				width={200}
-				theme={defaultSiteTheme === "dark" ? "outline" : "filled_black"}
 				text="continue_with"
 				logo_alignment="center"
 			/>
 		</div>
 	)
 }
-
-export default observer(GoogleSignIn)
