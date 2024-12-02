f32235ebff7dbb6dc53e267b41565977b9227da0
atamayev <75034340+actamayev@users.noreply.github.com>
2024-12-01 19:37:33 -0500

    Update mapped-nav-data.tsx

--- src/components/shadcn/sidebar/primary/mapped-nav-data.tsx
+++ src/components/shadcn/sidebar/primary/mapped-nav-data.tsx
@@ -42,7 +42,7 @@ export default function MappedNavData() {
 									isActive={location.pathname === item.url}
 									className="!flex !h-[60px] !w-[60px] !min-w-[60px] items-center
 									justify-center !p-0 group-data-[collapsible=icon]:!h-[60px] group-data-[collapsible=icon]:!w-[60px]
-									transition-all duration-300"
+									transition-all duration-300 hover:bg-zinc-200 dark:hover:bg-zinc-800"
 								>
 									<div className="flex h-[50px] w-[50px] !min-w-[50px] items-center justify-center">
 										<item.icon
