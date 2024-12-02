bd74b97849945f8a56ab6d07ae42ee0a914faba5
atamayev <75034340+actamayev@users.noreply.github.com>
2024-12-02 13:43:07 -0500

    Update product-showcase.tsx

--- src/components/home/discover/product-showcase.tsx
+++ src/components/home/discover/product-showcase.tsx
@@ -70,25 +70,6 @@ function ProductShowcase() {
 		<div className="w-full px-4 mb-10">
 			<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
 				<SectionCard
-					sectionTitle="Sandbox"
-					sectionSubtitle={
-						<div>
-							Freely control your robot with coding blocks for&nbsp;
-							<BoldSpanText extraClasses="font-medium">
-								limitless open-ended exploration&nbsp;
-							</BoldSpanText>
-							and experimentation.
-						</div>
-					}
-					cta="Jump into the Sandbox"
-					href="/sandbox"
-					Icon={TbSandbox}
-					extraClasses="bg-gradient-to-b from-orange-400 to-orange-50 dark:from-orange-200 dark:to-black"
-					linkToShow="bluedotrobots.com/sandbox"
-					imgSrc={siteTheme === "dark" ? "sandbox_dark.png" : "sandbox_light.png"}
-				/>
-
-				<SectionCard
 					sectionTitle="Lab"
 					sectionSubtitle={
 						<>
@@ -108,6 +89,25 @@ function ProductShowcase() {
 					extraClasses="bg-gradient-to-b from-emerald-400 to-emerald-50 dark:from-emerald-200 dark:to-black"
 					linkToShow="bluedotrobots.com/lab"
 					imgSrc={siteTheme === "dark" ? "lab_dark.png" : "lab_light.png"}
+				/>
+
+				<SectionCard
+					sectionTitle="Sandbox"
+					sectionSubtitle={
+						<div>
+							Freely control your robot with coding blocks for&nbsp;
+							<BoldSpanText extraClasses="font-medium">
+								limitless open-ended exploration&nbsp;
+							</BoldSpanText>
+							and experimentation.
+						</div>
+					}
+					cta="Jump into the Sandbox"
+					href="/sandbox"
+					Icon={TbSandbox}
+					extraClasses="bg-gradient-to-b from-orange-400 to-orange-50 dark:from-orange-200 dark:to-black"
+					linkToShow="bluedotrobots.com/sandbox"
+					imgSrc={siteTheme === "dark" ? "sandbox_dark.png" : "sandbox_light.png"}
 				/>
 			</div>
 		</div>
