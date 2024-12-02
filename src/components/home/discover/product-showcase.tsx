ccd5738af4a04e462212070d5b2004680a7f0f26
atamayev <75034340+actamayev@users.noreply.github.com>
2024-12-01 23:17:35 -0500

    Update product-showcase.tsx

--- src/components/home/discover/product-showcase.tsx
+++ src/components/home/discover/product-showcase.tsx
@@ -6,13 +6,14 @@ import { TbSandbox } from "react-icons/tb"
 import { ChevronRight } from "lucide-react"
 import Safari from "../../shadcn/ui/safari"
 import { cn } from "../../../lib/shadcn/utils"
+import { BoldSpanText } from "../bold-span-text"
 import { Card } from "@/components/shadcn/ui/card"
 import { Button } from "@/components/shadcn/ui/button"
 import useDefaultSiteTheme from "../../../hooks/memos/default-site-theme"
 
 interface ProductProps {
 	sectionTitle: string
-	sectionSubtitle: string
+	sectionSubtitle: React.ReactNode
 	cta: string
 	href: StaticPageNames
 	Icon: IconType
@@ -34,10 +35,7 @@ function SectionCard(props: ProductProps) {
 					<Icon className="size-9 origin-left transform-gpu transition-all duration-300 ease-in-out group-hover:scale-75" />
 					&nbsp;{sectionTitle}
 				</h2>
-				<p className="text-xl mb-4 opacity-80">
-					{sectionSubtitle}
-				</p>
-				<div className="flex items-center gap-6 mb-8">
+				<div className="flex items-center gap-6">
 					<Button variant="link" className="text-lg font-medium p-0 hover:no-underline">
 						<Link to={href}>
 							<div className={cn(
@@ -57,6 +55,9 @@ function SectionCard(props: ProductProps) {
 						src={imgSrc}
 					/>
 				</div>
+				<p className="text-2xl mt-4 dark:text-zinc-400">
+					{sectionSubtitle}
+				</p>
 			</div>
 		</Card>
 	)
@@ -70,8 +71,15 @@ function ProductShowcase() {
 			<div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
 				<SectionCard
 					sectionTitle="Sandbox"
-					sectionSubtitle="Creative freedom"
-					cta="See the sandbox"
+					sectionSubtitle={
+						<div>
+							Freely control your robot with coding blocks for&nbsp;
+							<BoldSpanText extraClasses="font-medium">
+							limitless open-ended exploration and experimentation.
+							</BoldSpanText>
+						</div>
+					}
+					cta="Jump into the Sandbox"
 					href="/sandbox"
 					Icon={TbSandbox}
 					extraClasses="bg-gradient-to-b from-orange-400 to-orange-50 dark:from-orange-200 dark:to-black"
@@ -81,8 +89,18 @@ function ProductShowcase() {
 
 				<SectionCard
 					sectionTitle="Lab"
-					sectionSubtitle="Learn in the lab"
-					cta="See the lab"
+					sectionSubtitle={
+						<div>
+							<BoldSpanText extraClasses="font-medium">
+							Explore guided tutorials, videos, and challenges&nbsp;
+							</BoldSpanText>
+							to learn about robot sensors and control in&nbsp;
+							<BoldSpanText extraClasses="font-medium">
+							a structured learning environment
+							</BoldSpanText>
+						</div>
+					}
+					cta="See the Lab"
 					href="/lab"
 					Icon={HiBeaker}
 					extraClasses="bg-gradient-to-b from-emerald-400 to-emerald-50 dark:from-emerald-200 dark:to-black"
