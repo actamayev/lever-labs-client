2ef85a0108396c78e78a03226be53d9055bc8699
atamayev <75034340+actamayev@users.noreply.github.com>
2024-12-02 22:59:31 -0500

    Remove cta

--- src/components/home/discover/product-showcase.tsx
+++ src/components/home/discover/product-showcase.tsx
@@ -1,21 +1,16 @@
-import { Link } from "react-router"
 import { observer } from "mobx-react"
 import { IconType } from "react-icons"
 import { HiBeaker } from "react-icons/hi"
 import { TbSandbox } from "react-icons/tb"
-import { ChevronRight } from "lucide-react"
 import Safari from "../../shadcn/ui/safari"
 import { cn } from "../../../lib/shadcn/utils"
 import { BoldSpanText } from "../bold-span-text"
 import { Card } from "@/components/shadcn/ui/card"
-import { Button } from "@/components/shadcn/ui/button"
 import useDefaultSiteTheme from "../../../hooks/memos/default-site-theme"
 
 interface ProductProps {
 	sectionTitle: string
 	sectionSubtitle: React.ReactNode
-	cta: string
-	href: StaticPageNames
 	Icon: IconType
 	extraClasses: string
 	imgSrc: string
@@ -23,31 +18,18 @@ interface ProductProps {
 }
 
 function SectionCard(props: ProductProps) {
-	const { sectionTitle, sectionSubtitle, cta, href, Icon, extraClasses, linkToShow, imgSrc } = props
+	const { sectionTitle, sectionSubtitle, Icon, extraClasses, linkToShow, imgSrc } = props
 
 	return (
 		<Card className={cn(
 			"overflow-hidden !border-0 rounded-none dark:text-black",
 			extraClasses
 		)}>
-			<div className="flex flex-col items-center text-center p-8 h-full">
-				<h2 className="text-4xl font-medium flex flex-row items-center">
+			<div className="flex flex-col items-center text-center p-5 h-full">
+				<h2 className="text-4xl font-medium flex flex-row items-center mb-5">
 					<Icon className="size-9 origin-left transform-gpu transition-all duration-300 ease-in-out group-hover:scale-75" />
 					&nbsp;{sectionTitle}
 				</h2>
-				<div className="flex items-center gap-6 my-2">
-					<Button variant="link" className="text-lg font-medium p-0 hover:no-underline">
-						<Link to={href}>
-							<div className={cn(
-								"flex flex-row items-center transition-all duration-200 no-underline",
-								"hover:underline hover:decoration-dotted dark:text-black"
-							)}>
-								{cta}
-								<ChevronRight className="h-4 w-4 ml-1" />
-							</div>
-						</Link>
-					</Button>
-				</div>
 				<div className="relative">
 					<Safari
 						url={linkToShow}
@@ -67,7 +49,7 @@ function ProductShowcase() {
 	const siteTheme = useDefaultSiteTheme()
 
 	return (
-		<div className="w-full px-4 mb-10">
+		<div className="w-full mb-10">
 			<div className="grid grid-cols-1 md:grid-cols-2 gap-4">
 				<SectionCard
 					sectionTitle="Lab"
@@ -77,14 +59,12 @@ function ProductShowcase() {
 							<BoldSpanText extraClasses="font-medium">
 							guided tutorials, videos, and challenges&nbsp;
 							</BoldSpanText>
-							to learn about robot sensors and control in&nbsp;
+								to learn about robot sensors and control in&nbsp;
 							<BoldSpanText extraClasses="font-medium">
 								a structured learning environment.
 							</BoldSpanText>
 						</>
 					}
-					cta="See the Lab"
-					href="/lab"
 					Icon={HiBeaker}
 					extraClasses="bg-gradient-to-b from-emerald-400 to-emerald-100 dark:from-emerald-200 dark:to-black"
 					linkToShow="bluedotrobots.com/lab"
@@ -102,8 +82,6 @@ function ProductShowcase() {
 							and experimentation.
 						</div>
 					}
-					cta="Jump into the Sandbox"
-					href="/sandbox"
 					Icon={TbSandbox}
 					extraClasses="bg-gradient-to-b from-orange-400 to-orange-100 dark:from-orange-200 dark:to-black"
 					linkToShow="bluedotrobots.com/sandbox"
