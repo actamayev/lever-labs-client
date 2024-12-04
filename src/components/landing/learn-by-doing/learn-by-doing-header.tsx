5927caec27df0c5dd29d639ea42b466591253a24
atamayev <75034340+actamayev@users.noreply.github.com>
2024-12-04 10:39:23 -0500

    Quote

--- src/components/landing/learn-by-doing/learn-by-doing-header.tsx
+++ src/components/landing/learn-by-doing/learn-by-doing-header.tsx
@@ -33,7 +33,7 @@ export default function LearnByDoing() {
 			</div>
 			<div className="w-1/2 flex items-center">
 				<TextRevealByWord
-					text="Blink an LED today, balance an inverted pendulum tomorrow."
+					text='"The best way to predict the future is to create it."¤- Alan Kay'
 					className="block"
 					instantTransition
 					wordClasses="text-6xl font-bold"
