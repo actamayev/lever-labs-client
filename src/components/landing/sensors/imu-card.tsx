266f8d19d6722837efc3e0961361f7eb7cdff7a2
atamayev <75034340+actamayev@users.noreply.github.com>
2024-12-04 15:59:32 -0500

    imu styles

--- src/components/landing/sensors/imu-card.tsx
+++ src/components/landing/sensors/imu-card.tsx
@@ -1,29 +1,49 @@
+import React, { useState } from "react"
 import { FaTachometerAlt } from "react-icons/fa"
 import SensorsSkeleton from "./sensors-skeleton"
 import { bentoIconSize } from "../../../utils/constants"
 import { DottedTextTooltip } from "../../dotted-underline-text"
 
+const vibrationStyles = `
+  @keyframes vibrate {
+    0% { transform: rotate(0deg); }
+    25% { transform: rotate(-2deg); }
+    75% { transform: rotate(2deg); }
+    100% { transform: rotate(0deg); }
+  }
+`
+
 export default function IMUCard() {
+	const [isHovered, setIsHovered] = useState(false)
+
 	return (
-		<SensorsSkeleton
-			title={(
-				<>
-					9-Axis&nbsp;
-					<DottedTextTooltip tooltipMessage="BNO085">
-						IMU
-					</DottedTextTooltip>
-				</>
-			)}
-			description="Seemlessly track orientation, acceleration, and motion"
-			icon={
-				<div className="w-fit">
-					<FaTachometerAlt
-						className="origin-left transition-all duration-300 text-black dark:text-white"
-						size={bentoIconSize}
-					/>
-				</div>
-			}
-			outerDivStyles="row-start-1 col-start-2 col-end-2"
-		/>
+		<>
+			<style>{vibrationStyles}</style>
+			<SensorsSkeleton
+				title={(
+					<>
+						9-Axis&nbsp;
+						<DottedTextTooltip tooltipMessage="BNO085">
+							IMU
+						</DottedTextTooltip>
+					</>
+				)}
+				description="Seemlessly track orientation, acceleration, and motion"
+				icon={
+					<div
+						className="w-fit group"
+						onMouseEnter={() => setIsHovered(true)}
+						onMouseLeave={() => setIsHovered(false)}
+					>
+						<FaTachometerAlt
+							className={`origin-center text-black dark:text-white transform
+                ${isHovered ? "animate-[vibrate_100ms_ease-in-out_infinite]" : ""}`}
+							size={bentoIconSize}
+						/>
+					</div>
+				}
+				outerDivStyles="row-start-1 col-start-2 col-end-2"
+			/>
+		</>
 	)
 }
