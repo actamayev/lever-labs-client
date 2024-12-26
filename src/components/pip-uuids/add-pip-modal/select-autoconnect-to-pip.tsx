445eea88dd5bec726dd4466246da394f58b953d5
atamayev <75034340+actamayev@users.noreply.github.com>
2024-12-26 15:39:30 -0500

    Update select-autoconnect-to-pip.tsx

--- src/components/pip-uuids/add-pip-modal/select-autoconnect-to-pip.tsx
+++ src/components/pip-uuids/add-pip-modal/select-autoconnect-to-pip.tsx
@@ -15,47 +15,51 @@ import { Button } from "../../shadcn/ui/button"
 function SelectAutoreconnectToPip({ control } : { control: Control<IncompletePipData>}) {
 	const pipClass = usePipContext()
 
-	if (pipClass.addingNewPipRequirements.isPipOnline) return null
+	if (!pipClass.addingNewPipRequirements.isPipOnline) return null
 
 	return (
-		<FormField
-			control={control}
-			name="shouldAutoConnect"
-			render={({ field }) => (
-				<FormItem className="flex items-center justify-between space-x-2">
-					<div className="flex flex-row space-x-2">
-						<FormLabel className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
+		<div className="mt-2">
+			<FormField
+				control={control}
+				name="shouldAutoConnect"
+				render={({ field }) => (
+					<FormItem className="flex items-center justify-between space-x-2">
+						<div className="flex items-center space-x-2">
+							<FormLabel className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed
+						peer-disabled:opacity-70 flex items-center">
 							Auto-connect
-						</FormLabel>
-						<TooltipProvider delayDuration={0}>
-							<Tooltip>
-								<TooltipTrigger asChild>
-									<Button
-										type="button"
-										variant="ghost"
-										size="sm"
-										className="h-auto p-1 dark:hover:bg-zinc-700"
-									>
-										<Info size={15}/>
-									</Button>
-								</TooltipTrigger>
-								<TooltipContent side="top">
+							</FormLabel>
+							<TooltipProvider delayDuration={0}>
+								<Tooltip>
+									<TooltipTrigger asChild>
+										<Button
+											type="button"
+											variant="ghost"
+											size="sm"
+											className="h-auto p-1 dark:hover:bg-zinc-700 flex items-center"
+										>
+											<Info size={15}/>
+										</Button>
+									</TooltipTrigger>
+									<TooltipContent side="top">
 									This Pip is currently online. Auto-connect will connect you to this Pip
-								</TooltipContent>
-							</Tooltip>
-						</TooltipProvider>
-					</div>
-					<FormControl>
-						<Slider
-							id="pip-auto-connect-slider"
-							checkedCondition={field.value}
-							onChangeCheckedCondition={() => field.onChange(!field.value)}
-							colorChangeOnToggle={true}
-						/>
-					</FormControl>
-				</FormItem>
-			)}
-		/>
+									</TooltipContent>
+								</Tooltip>
+							</TooltipProvider>
+						</div>
+						<FormControl className="flex items-center">
+							<Slider
+								id="pip-auto-connect-slider"
+								checkedCondition={field.value}
+								onChangeCheckedCondition={() => field.onChange(!field.value)}
+								colorChangeOnToggle={true}
+							/>
+						</FormControl>
+					</FormItem>
+				)}
+			/>
+		</div>
+
 	)
 }
 
