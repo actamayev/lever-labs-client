465b3a5c6da0353ec468aed68fe9561bd00c524a
atamayev <75034340+actamayev@users.noreply.github.com>
2024-12-27 22:37:55 -0500

    Update available-pips-dropdown.tsx

--- src/components/pip-uuids/available-pips-dropdown.tsx
+++ src/components/pip-uuids/available-pips-dropdown.tsx
@@ -1,7 +1,7 @@
 import _ from "lodash"
-import { useMemo } from "react"
 import { Plus } from "lucide-react"
 import { observer } from "mobx-react"
+import { useCallback, useMemo } from "react"
 import { cn } from "../../lib/shadcn/utils"
 import { Button } from "../shadcn/ui/button"
 import SingleAvailablePip from "./single-available-pip"
@@ -17,7 +17,10 @@ function AvailablePipsDropdown() {
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [pipClass.selectedPip, pipClass.pipData.length])
 
-	if (_.isNull(addPipClass)) return null
+	const closeModal = useCallback(() => {
+		if (_.isNull(addPipClass)) return
+		addPipClass.store.setIsAppPipModalOpen(true)
+	}, [addPipClass])
 
 	return (
 		<>
@@ -28,7 +31,7 @@ function AvailablePipsDropdown() {
 				/>
 			))}
 			<Button
-				onClick={() => addPipClass.store.setIsAppPipModalOpen(true)}
+				onClick={closeModal}
 				className={cn(
 					"w-full px-4 py-2 h-7 flex items-center gap-2 text-black dark:text-white",
 					"bg-zinc-100 hover:bg-zinc-300 dark:bg-zinc-800 dark:hover:bg-zinc-600 cursor-pointer"
