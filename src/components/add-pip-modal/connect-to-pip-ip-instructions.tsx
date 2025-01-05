ab0aad65e1085ec205410350b2bffe35adb1233f
atamayev <75034340+actamayev@users.noreply.github.com>
2025-01-05 16:17:35 -0500

    Create separate open ip addr tab callback

--- src/components/add-pip-modal/connect-to-pip-ip-instructions.tsx
+++ src/components/add-pip-modal/connect-to-pip-ip-instructions.tsx
@@ -1,61 +1,12 @@
-/* eslint-disable max-lines-per-function */
 import _ from "lodash"
-import { useCallback } from "react"
 import { observer } from "mobx-react"
 import { Button } from "../shadcn/ui/button"
-import useStyledToast from "../toast-options"
-import usePipStatusPoll from "../../hooks/pip/pip-status-poll"
 import { useAddPipContext } from "../../contexts/add-pip-context"
+import useOpenIpAddrTab from "../../hooks/pip/open-ip-addr-tab"
 
 function ConnectToPipInstructions() {
-	const toast = useStyledToast()
 	const addPipClass = useAddPipContext()
-	const pipStatusPoll = usePipStatusPoll()
-
-	const openIpAddrTab = useCallback(async () => {
-		if (_.isNull(addPipClass)) return
-
-		try {
-			// Try to reach Cloudflare to check internet connectivity
-			await fetch("https://cloudflare.com/cdn-cgi/trace", {
-				method: "GET",
-				cache: "no-cache"
-			})
-
-			// If we get here, we're still connected to regular internet
-			console.info("User still connected to Wi-Fi, not opening Pip connection window")
-			addPipClass.store.setIsUserReadyToConnectToPipDialog(false)
-			return
-
-		} catch (error) {
-			// Network error means we're likely connected to the Pip's AP
-			console.error("Network error - potentially connected to Pip AP:", error)
-
-			try {
-				addPipClass.store.setIsUserReadyToConnectToPipDialog(true)
-				addPipClass.store.setNewPipConnectionStatus("connecting")
-
-				const newWindow = window.open(
-					`http://192.168.4.1/setup?credentials=${addPipClass.store.encodedWifiCredentials}`,
-					"_blank",
-					"width=400,height=300"
-				)
-
-				if (!newWindow) {
-					throw new Error("Popup was blocked. Please allow popups for this site and try again.")
-				}
-
-				pipStatusPoll()
-
-			} catch (popupError) {
-				console.error("Failed to open setup window:", popupError)
-				toast.negative({
-					title: `Unable to connect ${addPipClass.store.mirroredFormValues.pipName} to Wi-Fi`,
-					description: "Please ensure popups are allowed and try again"
-				})
-			}
-		}
-	}, [addPipClass, pipStatusPoll, toast])
+	const openIpAddrTab = useOpenIpAddrTab()
 
 	if (
 		_.isNull(addPipClass) ||
