df506a702e748904b1d1367e2780487ed2835d79
atamayev <75034340+actamayev@users.noreply.github.com>
2025-01-10 14:45:01 -0500

    Update welcome-sidebar-section.tsx

--- src/components/shadcn/sidebar/secondary/lab/welcome-sidebar-section.tsx
+++ src/components/shadcn/sidebar/secondary/lab/welcome-sidebar-section.tsx
@@ -1,15 +1,22 @@
 import { House } from "lucide-react"
+import { useLocation } from "react-router"
 import { SidebarGroup, SidebarGroupLabel } from "@/components/shadcn/ui/sidebar"
 import useTypedNavigate from "../../../../../hooks/navigate/typed-navigate"
 
 export default function WelcomeSidebarSection() {
 	const navigate = useTypedNavigate()
+	const location = useLocation()
+
+	const isActive = location.pathname === "/lab/welcome"
 
 	return (
 		<SidebarGroup>
 			<SidebarGroupLabel
-				className="text-xl dark:text-white text-black cursor-pointer
-				hover:bg-zinc-100 dark:hover:bg-zinc-800 transition-all"
+				className={`
+					text-xl dark:text-white text-black cursor-pointer
+					transition-all
+					${isActive ? "bg-zinc-100 dark:bg-zinc-800" : "hover:bg-zinc-100 dark:hover:bg-zinc-800"}
+					`}
 				onClick={() => navigate("/lab/welcome")}
 			>
 				<div className="space-x-2 flex flex-row">
