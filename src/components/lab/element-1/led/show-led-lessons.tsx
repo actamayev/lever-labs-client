0962eccd311e242b807ef1c75f7233c725e8a903
atamayev <75034340+actamayev@users.noreply.github.com>
2025-01-26 12:16:01 -0500

    Simplify code

--- src/components/lab/element-1/led/show-led-lessons.tsx
+++ src/components/lab/element-1/led/show-led-lessons.tsx
@@ -1,66 +1,53 @@
+import { useMemo } from "react"
 import ledLessons from "./led-lessons-object"
 import Lilypad from "../../lab-structure/lilypad/lilypad"
 import setLessonVerticalPosition from "../../../../utils/lab/set-lesson-vertical-position"
 
+// Helper function to get vertical position for stacked items
+const getStackPosition = (index: number, defaultPosition: VerticalPosition): VerticalPosition => {
+	if (!index) return defaultPosition  // First item uses its default position
+	return index === 1 ? 5 : 9         // Second item at 5, third at 9
+}
+
 export default function ShowLEDLessons() {
-	const groupedLedLessons = ledLessons.reduce((acc, lesson) => {
-		if (lesson.stackWithPrevious) {
-			// Get the last group
-			const lastGroup = acc[acc.length - 1]
-			// Add this lesson to the existing group
-			lastGroup.lessons.push(lesson)
+	const groups = useMemo(() => {
+		return ledLessons.reduce<Array<typeof ledLessons>>((acc, lesson) => {
+			if (lesson.stackWithPrevious) {
+				acc[acc.length - 1].push(lesson)
+			} else {
+				acc.push([lesson])
+			}
 			return acc
-		} else {
-			// Create a new group with this lesson
-			acc.push({
-				lessons: [lesson],
-				position: acc.length
-			})
-			return acc
-		}
-	}, [] as { lessons: typeof ledLessons, position: number }[])
+		}, [])
+	}, [])
 
 	return (
 		<>
-			{groupedLedLessons.map((group, groupIndex) => {
-				// Get the height needed for container based on last item's position
-				const lastItem = group.lessons[group.lessons.length - 1]
-				const containerHeight = lastItem.stackWithPrevious
-					? setLessonVerticalPosition(9)
-					: setLessonVerticalPosition(lastItem.verticalPosition)
-
-				return (
-					<div
-						key={groupIndex}
-						className="relative mr-56"
-						style={{ height: containerHeight }}
-					>
-						{group.lessons.map((lesson, lessonIndex) => {
-							let effectivePosition
-							if (lesson.stackWithPrevious) {
-								// If it's the second item in stack, align with position 5
-								// If it's the third item in stack, align with position 9
-								// eslint-disable-next-line no-nested-ternary
-								effectivePosition = lessonIndex === 1 ? 5 : lessonIndex === 2 ? 9 : lesson.verticalPosition
-							} else {
-								effectivePosition = lesson.verticalPosition
-							}
-
-							return (
-								<div
-									key={lessonIndex}
-									className="absolute"
-									style={{
-										top: setLessonVerticalPosition(effectivePosition)
-									}}
-								>
-									<Lilypad lesson={lesson} />
-								</div>
-							)
-						})}
-					</div>
-				)
-			})}
+			{groups.map((groupLessons) => (
+				<div
+					key={groupLessons[0].lessonUrl}
+					className="relative mr-56"
+					style={{
+						height: setLessonVerticalPosition(groupLessons.length > 1 ? 9 : groupLessons[0].verticalPosition)
+					}}
+				>
+					{groupLessons.map((lesson, index) => (
+						<div
+							key={lesson.lessonUrl}
+							className="absolute"
+							style={{
+								top: setLessonVerticalPosition(
+									(lesson.stackWithPrevious
+										? getStackPosition(index, lesson.verticalPosition)
+										: lesson.verticalPosition)
+								)
+							}}
+						>
+							<Lilypad lesson={lesson} />
+						</div>
+					))}
+				</div>
+			))}
 		</>
 	)
 }
