43f1d3d3931d1a3aa52e35bedbb448524bab985c
atamayev <75034340+actamayev@users.noreply.github.com>
2025-01-26 11:51:04 -0500

    Update set-lesson-vertical-position.ts

--- src/utils/lab/set-lesson-vertical-position.ts
+++ src/utils/lab/set-lesson-vertical-position.ts
@@ -1,14 +1,14 @@
 export default function setLessonVerticalPosition(verticalPosition: VerticalPosition): string {
 	switch (verticalPosition) {
-	case 1: return ""          // Base level
-	case 2: return "60px"      // +60px from base
-	case 3: return "112px"     // +52px
-	case 4: return "170px"     // +58px
-	case 5: return "225px"     // +55px
-	case 6: return "280px"     // +55px
-	case 7: return "337px"     // +57px
-	case 8: return "395px"     // +58px
-	case 9: return "450px"     // +55px
-	default: return ""         // Fallback
+	case 1: return "0px"
+	case 2: return "60px"
+	case 3: return "120px"
+	case 4: return "180px"
+	case 5: return "240px"
+	case 6: return "300px"
+	case 7: return "360px"
+	case 8: return "420px"
+	case 9: return "480px"
+	default: return "0px"
 	}
 }
