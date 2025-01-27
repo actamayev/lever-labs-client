6fcb829323b8d9c7828062eb46ef68dcb0c81697
atamayev <75034340+actamayev@users.noreply.github.com>
2025-01-26 19:18:30 -0500

    Finish abstracting sandbox

--- src/components/blockly-component.tsx
+++ src/components/blockly-component.tsx
@@ -1,10 +1,12 @@
-import { useCallback, useEffect, useMemo, useRef, useState } from "react"
 import * as Blockly from "blockly"
+import { isNull } from "lodash-es"
 import { observer } from "mobx-react"
 import { BlocklyWorkspace } from "react-blockly"
+import { useCallback, useEffect, useMemo, useRef, useState } from "react"
 import { cppGenerator } from "../utils/cpp/cpp-generator"
-import getWorkspaceConfig from "../utils/blockly/workspace-config"
 import useDefaultSiteTheme from "../hooks/memos/default-site-theme"
+import useInitializeBlocks from "../hooks/blockly/initialize-blocks"
+import getWorkspaceConfig, { darkTheme, lightTheme } from "../utils/blockly/workspace-config"
 
 const initialXml = `
     <xml xmlns="https://developers.google.com/blockly/xml"/>
@@ -13,7 +15,6 @@ const initialXml = `
 interface Props {
 	toolboxConfig: Blockly.utils.toolbox.ToolboxDefinition
 	setCppCode: React.Dispatch<React.SetStateAction<string>>
-
 }
 
 function EnhancedBlocklyComponent (props: Props) {
@@ -23,10 +24,22 @@ function EnhancedBlocklyComponent (props: Props) {
 	const isDarkMode = defaultSiteTheme === "dark"
 	const containerRef = useRef<HTMLDivElement>(null)
 	const workspaceRef = useRef<Blockly.WorkspaceSvg | null>(null)
+	const initializeBlocks = useInitializeBlocks()
 
 	const workspaceConfiguration = useMemo(() => {
 		return getWorkspaceConfig(isDarkMode)
 	}, [isDarkMode])
+
+	const handleWorkspaceChange = useCallback((workspace: Blockly.WorkspaceSvg) => {
+		workspaceRef.current = workspace
+		const newXml = Blockly.Xml.domToText(
+			Blockly.Xml.workspaceToDom(workspace)
+		)
+		const cppCode = cppGenerator.workspaceToCode(workspace)
+
+		setBlocklyXml(newXml)
+		setCppCode(cppCode)
+	}, [setCppCode])
 
 	useEffect(() => {
 		if (!containerRef.current) return
@@ -44,16 +57,33 @@ function EnhancedBlocklyComponent (props: Props) {
 		}
 	}, [])
 
-	const handleWorkspaceChange = useCallback((workspace: Blockly.WorkspaceSvg) => {
-		workspaceRef.current = workspace
-		const newXml = Blockly.Xml.domToText(
-			Blockly.Xml.workspaceToDom(workspace)
-		)
-		const cppCode = cppGenerator.workspaceToCode(workspace)
+	useEffect(() => {
+		if (workspaceRef.current) {
+			workspaceRef.current.setTheme(isDarkMode ? darkTheme : lightTheme)
+		}
+	}, [isDarkMode])
 
-		setBlocklyXml(newXml)
-		setCppCode(cppCode)
-	}, [setCppCode])
+	const disableFlyoutAutoclose = useCallback(() => {
+		if (!workspaceRef.current) return // Exit if workspace isn't ready
+
+		const toolbox = workspaceRef.current.getToolbox()
+		// const workspace = Blockly.getMainWorkspace() as Blockly.WorkspaceSvg
+
+		// const toolbox = workspace.getToolbox()
+		if (!toolbox) return
+
+		const flyout = toolbox.getFlyout()
+		if (isNull(flyout)) return
+		flyout.autoClose = false
+	}, [])
+
+	useEffect(() => {
+		initializeBlocks()
+		disableFlyoutAutoclose()
+		// TODO: Fix, not working
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		// (Blockly.Tooltip as any).HOVER_MS = 0 // Set the tooltip delay to be instant
+	}, [initializeBlocks, disableFlyoutAutoclose])
 
 	return (
 		<div
--- src/components/sandbox/sandbox-blockly-component.tsx
+++ src/components/sandbox/sandbox-blockly-component.tsx
@@ -1,15 +1,10 @@
-import isNull from "lodash-es/isNull"
-import * as Blockly from "blockly"
 import isEmpty from "lodash-es/isEmpty"
 import { observer } from "mobx-react"
-import { useState, useEffect, useCallback, useRef, lazy, Suspense } from "react"
+import { useState, useCallback, lazy, Suspense } from "react"
 import { Button } from "../shadcn/ui/button"
 import { usePipContext } from "../../contexts/pip-context"
 import useSendCppToPip from "../../hooks/pip/send-cpp-to-pip"
 import { toolboxConfig } from "../../utils/blockly/toolbox-config"
-import useDefaultSiteTheme from "../../hooks/memos/default-site-theme"
-import useInitializeBlocks from "../../hooks/blockly/initialize-blocks"
-import { darkTheme, lightTheme } from "../../utils/blockly/workspace-config"
 
 const BlocklyComponent = lazy(() => import("../blockly-component"))
 
@@ -17,55 +12,6 @@ function SandboxBlocklyComponent() {
 	const [cppCode, setCppCode] = useState("")
 	const pipClass = usePipContext()
 	const sendCppToPip = useSendCppToPip()
-	const initializeBlocks = useInitializeBlocks()
-	const defaultSiteTheme = useDefaultSiteTheme()
-	const isDarkMode = defaultSiteTheme === "dark"
-	const workspaceRef = useRef<Blockly.WorkspaceSvg | null>(null)
-	const containerRef = useRef<HTMLDivElement>(null)
-
-	useEffect(() => {
-		if (!containerRef.current) return
-
-		const resizeObserver = new ResizeObserver(() => {
-			if (workspaceRef.current) {
-				Blockly.svgResize(workspaceRef.current)
-			}
-		})
-
-		resizeObserver.observe(containerRef.current)
-
-		return () => {
-			resizeObserver.disconnect()
-		}
-	}, [])
-
-	useEffect(() => {
-		if (workspaceRef.current) {
-			workspaceRef.current.setTheme(isDarkMode ? darkTheme : lightTheme)
-		}
-	}, [isDarkMode])
-
-	const disableFlyoutAutoclose = useCallback(() => {
-		if (!workspaceRef.current) return // Exit if workspace isn't ready
-
-		const toolbox = workspaceRef.current.getToolbox()
-		// const workspace = Blockly.getMainWorkspace() as Blockly.WorkspaceSvg
-
-		// const toolbox = workspace.getToolbox()
-		if (!toolbox) return
-
-		const flyout = toolbox.getFlyout()
-		if (isNull(flyout)) return
-		flyout.autoClose = false
-	}, [])
-
-	useEffect(() => {
-		initializeBlocks()
-		disableFlyoutAutoclose()
-		// TODO: Fix, not working
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		// (Blockly.Tooltip as any).HOVER_MS = 0 // Set the tooltip delay to be instant
-	}, [initializeBlocks, disableFlyoutAutoclose])
 
 	const sendCodeToCppCallback = useCallback(async () => {
 		await sendCppToPip(cppCode)
