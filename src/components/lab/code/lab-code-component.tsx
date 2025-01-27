aca11fd102e151f8e42b658d85875030c3c46fb6
atamayev <75034340+actamayev@users.noreply.github.com>
2025-01-26 20:26:47 -0500

    Update lab-code-component.tsx

--- src/components/lab/code/lab-code-component.tsx
+++ src/components/lab/code/lab-code-component.tsx
@@ -4,18 +4,18 @@ import ActivityTemplate from "../activity-structure/activity-template"
 import { Card, CardContent, CardHeader, CardTitle } from "../../shadcn/ui/card"
 
 interface Props {
-	lessonTitle: string
-	previousPageLink: LabPages
-	previousPageActivity: ActivityType
-	nextPageLink: LabPages
-	nextPageActivity: ActivityType
-	element: 1 | 2 | 3
-	lessonIcon: ReactElement
-	progressPercent: number
-	isNextPageDemo?: boolean
-	codingTitle: string
-	codingDescription: string
-	toolboxConfig: Blockly.utils.toolbox.ToolboxDefinition
+    lessonTitle: string
+    previousPageLink: LabPages
+    previousPageActivity: ActivityType
+    nextPageLink: LabPages
+    nextPageActivity: ActivityType
+    element: 1 | 2 | 3
+    lessonIcon: ReactElement
+    progressPercent: number
+    isNextPageDemo?: boolean
+    codingTitle: string
+    codingDescription: string
+    toolboxConfig: Blockly.utils.toolbox.ToolboxDefinition
 }
 
 const BlocklyComponent = lazy(() => import("../../blockly-component"))
@@ -35,6 +35,7 @@ export default function LabCodeComponent(props: Props) {
 		toolboxConfig,
 		isNextPageDemo = false
 	} = props
+
 	return (
 		<ActivityTemplate
 			element={element}
@@ -48,15 +49,19 @@ export default function LabCodeComponent(props: Props) {
 			isNextPageDemo={isNextPageDemo}
 			extraClasses="h-screen"
 		>
-			<Card>
-				<CardHeader>
-					<CardTitle className="text-6xl font-bold">{codingTitle}</CardTitle>
-				</CardHeader>
-				<CardContent className="text-xl font-medium">{codingDescription}</CardContent>
-			</Card>
-			<Suspense>
-				<BlocklyComponent toolboxConfig={toolboxConfig}/>
-			</Suspense>
+			<main className="flex-1 flex flex-col p-4 justify-center max-w-4xl">
+				<Card>
+					<CardHeader>
+						<CardTitle className="text-6xl font-bold">{codingTitle}</CardTitle>
+					</CardHeader>
+					<CardContent className="text-xl font-medium">{codingDescription}</CardContent>
+				</Card>
+				<div className="flex-1 min-h-0 mt-4">
+					<Suspense>
+						<BlocklyComponent toolboxConfig={toolboxConfig} />
+					</Suspense>
+				</div>
+			</main>
 		</ActivityTemplate>
 	)
 }
