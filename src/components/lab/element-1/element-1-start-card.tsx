897bbd6f9fc87824fba94aee50d1cec5739efb33
atamayev <75034340+actamayev@users.noreply.github.com>
2025-01-27 21:49:30 -0500

    Update element-1-start-card.tsx

--- src/components/lab/element-1/element-1-start-card.tsx
+++ src/components/lab/element-1/element-1-start-card.tsx
@@ -2,16 +2,54 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../..
 
 export default function Element1StartCard() {
 	return (
-		<Card className="w-2/5 p-4 flex flex-col m-4 rounded-lg">
+		<Card className="w-[600px] p-4 flex flex-col m-4 rounded-lg">
 			<CardHeader>
 				<CardTitle>
 					<h1 className="text-4xl font-bold mb-6">Element 1: Sensor Basics</h1>
 				</CardTitle>
-			</CardHeader>
-			<CardContent>
-				<CardDescription className="text-xl font-medium">
-					Test description
+				<CardDescription className="text-xl">
+					Welcome to the Lab! We&apos;re happy you&apos;re here.
 				</CardDescription>
+			</CardHeader>
+			<CardContent className="space-y-6">
+				{/* Getting Started Section */}
+				<div className="space-y-2 border">
+					<h2 className="text-lg font-semibold text-zinc-800 dark:text-zinc-200">
+						Getting Started
+					</h2>
+					<p className="text-lg text-zinc-600 dark:text-zinc-400">
+						First: if you have a Pip, we recommend getting it connected to the internet (it&apos;s quick & easy)
+					</p>
+				</div>
+
+				{/* In This Element Section */}
+				<div className="space-y-2">
+					<h2 className="text-lg font-semibold text-zinc-800 dark:text-zinc-200">
+						In This Element:
+					</h2>
+					<p className="text-lg text-zinc-600 dark:text-zinc-400">
+						We&apos;ll investigate how Pip&apos;s sensors actually work under the surface, their applications,
+						and you&apos;ll learn how to control them. You&apos;ll work with:
+					</p>
+					<ul className="list-disc pl-6 text-lg text-zinc-600 dark:text-zinc-400">
+						<li>LEDs</li>
+						<li>Motors</li>
+						<li>Buttons</li>
+						<li>Distance sensors</li>
+						<li>IR sensors</li>
+						<li>And much more</li>
+					</ul>
+				</div>
+
+				{/* After Completing Section */}
+				<div className="space-y-2">
+					<h2 className="text-lg font-semibold text-zinc-800 dark:text-zinc-200">
+						After Completing This Element:
+					</h2>
+					<p className="text-lg text-zinc-600 dark:text-zinc-400">
+						You&apos;ll be able to read, control, and understand how each of Pip&apos;s sensors works.
+					</p>
+				</div>
 			</CardContent>
 		</Card>
 	)
