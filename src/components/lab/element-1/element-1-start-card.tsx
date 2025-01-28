8fc5eb00593cc7a7f6b4ea49df83f48c2aa90e4a
atamayev <75034340+actamayev@users.noreply.github.com>
2025-01-28 15:01:08 -0500

    Update element-1-start-card.tsx

--- src/components/lab/element-1/element-1-start-card.tsx
+++ src/components/lab/element-1/element-1-start-card.tsx
@@ -64,11 +64,11 @@ export default function Element1StartCard() {
 				<div className="space-y-2 p-4 border border-zinc-200 dark:border-zinc-700 rounded-lg
 					bg-zinc-50 dark:bg-zinc-800/50">
 					<div className="grid grid-cols-2 gap-4">
-						<div className="text-lg text-zinc-600 dark:text-zinc-400 flex flex-row items-center justify-center gap-2">
+						<div className="text-lg text-zinc-600 dark:text-zinc-200 flex flex-row items-center justify-center gap-2">
 							<CircuitBoard />
 							9 Peripherals
 						</div>
-						<div className="text-lg text-zinc-600 dark:text-zinc-400 flex flex-row items-center justify-center gap-2">
+						<div className="text-lg text-zinc-600 dark:text-zinc-200 flex flex-row items-center justify-center gap-2">
 							<Hourglass />
 							10 hours
 						</div>
