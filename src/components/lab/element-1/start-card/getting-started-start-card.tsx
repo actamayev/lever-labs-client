69dd084a677f6ff2b10b5f01fa43d562e7e9d3c9
atamayev <75034340+actamayev@users.noreply.github.com>
2025-01-30 17:08:51 -0500

    Add pip styles

--- src/components/lab/element-1/start-card/getting-started-start-card.tsx
+++ src/components/lab/element-1/start-card/getting-started-start-card.tsx
@@ -1,5 +1,6 @@
 import { observer } from "mobx-react"
 import isEmpty from "lodash-es/isEmpty"
+import GettingStartedAddPip from "./getting-started-add-pip"
 import { usePipContext } from "../../../../contexts/pip-context"
 
 function GettingStartedStartCard() {
@@ -18,15 +19,19 @@ function GettingStartedStartCard() {
 	}
 
 	return (
-		<div className="space-y-2 p-4 border border-zinc-200 dark:border-zinc-700 rounded-lg
-        bg-zinc-50 dark:bg-zinc-800/50">
-			<h2 className="text-lg font-semibold text-zinc-800 dark:text-zinc-200">
-                Getting Started
-			</h2>
-			{/* add a button that allows the user to do so */}
-			<p className="text-lg text-zinc-600 dark:text-zinc-400">
-				{getCardContent()}
-			</p>
+		<div className="border border-zinc-200 dark:border-zinc-700 rounded-lg
+            bg-zinc-50 dark:bg-zinc-800/50">
+			<div className="flex items-start justify-between gap-4">
+				<div className="space-y-2 flex-1 p-4">
+					<h2 className="text-lg font-semibold text-zinc-800 dark:text-zinc-200">
+                        Getting Started
+					</h2>
+					<p className="text-lg text-zinc-600 dark:text-zinc-400">
+						{getCardContent()}
+					</p>
+				</div>
+				<GettingStartedAddPip />
+			</div>
 		</div>
 	)
 }
