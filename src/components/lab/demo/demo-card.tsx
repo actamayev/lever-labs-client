48e72c1c34b02e74a78ca6ca6c69ad61d1544086
atamayev <75034340+actamayev@users.noreply.github.com>
2025-02-04 20:44:01 -0500

    Move component

--- src/components/lab/demo/demo-card.tsx
+++ src/components/lab/demo/demo-card.tsx
@@ -1,6 +1,7 @@
+import DemoButton from "./demo-button"
+import InThisDemo from "./in-this-demo"
 import { Card, CardContent, CardHeader, CardTitle } from "../../shadcn/ui/card"
 import GettingStartedStartCard from "../element-1/start-card/getting-started-start-card"
-import DemoButton from "./demo-button"
 
 interface Props {
     lessonDemoTitle: string
@@ -20,28 +21,7 @@ export default function DemoCard(props: Props) {
 			</CardHeader>
 			<CardContent className="space-y-6">
 				<GettingStartedStartCard />
-				<div className="space-y-2 p-4 border border-zinc-200 dark:border-zinc-700 rounded-lg
-                    bg-zinc-50 dark:bg-zinc-800/50">
-					<h2 className="text-lg font-semibold text-zinc-800 dark:text-zinc-200">
-                        In This Demo, you&apos;ll:
-					</h2>
-					<div className="grid grid-cols-2 gap-4">
-						<ul className="list-disc pl-6 text-lg text-zinc-600 dark:text-zinc-400">
-							{demoDeliverables.slice(0, 2).map((deliverable, index) => (
-								<li key={index}>
-									{deliverable}
-								</li>
-							))}
-						</ul>
-						<ul className="list-disc pl-6 text-lg text-zinc-600 dark:text-zinc-400">
-							{demoDeliverables.slice(2, 4).map((deliverable, index) => (
-								<li key={`second-${index}`}>
-									{deliverable}
-								</li>
-							))}
-						</ul>
-					</div>
-				</div>
+				<InThisDemo demoDeliverables={demoDeliverables} />
 
 				{/* Demos Grid */}
 				<div className="grid grid-cols-2 gap-4">
