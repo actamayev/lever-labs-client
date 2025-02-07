ca3bf40a750d2d0ba3064be4eb0206c2a258d6db
atamayev <75034340+actamayev@users.noreply.github.com>
2025-02-06 23:33:08 -0500

    Update getting-started-add-pip.tsx

--- src/components/lab/element-1/start-card/getting-started-add-pip.tsx
+++ src/components/lab/element-1/start-card/getting-started-add-pip.tsx
@@ -26,7 +26,7 @@ function GettingStartedAddPip() {
 					<Button
 						onClick={clickPipSidebarButton}
 						className="!flex !h-24 !w-24 !min-w-24 relative items-center justify-center
-                        bg-zinc-50 dark:bg-zinc-800/50 hover:bg-zinc-100 dark:hover:bg-zinc-800
+                        bg-zinc-100/50 dark:bg-zinc-800/50 hover:bg-zinc-100 dark:hover:bg-zinc-800
                         duration-200 border-zinc-200 dark:border-zinc-700 border-l rounded-none rounded-tr-lg rounded-br-lg shadow-none"
 					>
 						<Bot className="!h-12 !w-12 !min-w-12 text-black dark:text-white" />
