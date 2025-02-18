29e43942462a9cab3f43233827824d3e289a40cc
atamayev <75034340+actamayev@users.noreply.github.com>
2025-02-18 13:33:57 -0500

    Simplify code

--- src/components/lab/element-1/led/show-led-lessons.tsx
+++ src/components/lab/element-1/led/show-led-lessons.tsx
@@ -1,13 +1,13 @@
-import { useMemo, useRef, useEffect, useState } from "react"
+import { useRef, useEffect, useState } from "react"
 import ledLessons from "./led-lessons-object"
 import Lilypad from "../../lab-structure/lilypad/lilypad"
 import PathTickMark from "../../lab-structure/path-tick-mark"
 import setLessonVerticalPosition from "../../../../utils/lab/set-lesson-vertical-position"
 
 interface LilyPadPositions {
-	x: number
-	y: number
-	arcDirection: ArcDirection
+    x: number
+    y: number
+    arcDirection: ArcDirection
 }
 
 export default function ShowLEDLessons() {
@@ -15,15 +15,10 @@ export default function ShowLEDLessons() {
 	const containerRef = useRef<HTMLDivElement>(null)
 	const lilypadSectionRef = useRef<HTMLDivElement>(null)
 
-	const groups = useMemo(() => {
-		return ledLessons.map(lesson => [lesson])
-	}, [])
-
 	useEffect(() => {
 		if (!containerRef.current || !lilypadSectionRef.current) return
 
 		const positions: LilyPadPositions[] = []
-
 		const lilypads = containerRef.current.querySelectorAll("[data-lilypad-icon]")
 		const lilypadSectionRect = lilypadSectionRef.current.getBoundingClientRect()
 
@@ -39,43 +34,35 @@ export default function ShowLEDLessons() {
 		})
 
 		setLilypadPositions(positions)
-	}, [groups])
-
-	const renderablePositions = useMemo(() => [...lilypadPositions], [lilypadPositions])
+	}, [])
 
 	return (
 		<div ref={containerRef} className="flex">
 			<div ref={lilypadSectionRef} className="relative">
-				{renderablePositions.slice(0, -1).map((pos, i) => (
+				{lilypadPositions.slice(0, -1).map((pos, i) => (
 					<PathTickMark
 						key={i}
 						startPosition={pos}
-						endPosition={renderablePositions[i + 1]}
+						endPosition={lilypadPositions[i + 1]}
 						arcDirection={pos.arcDirection}
 					/>
 				))}
 
 				<div className="flex">
-					{groups.map((groupLessons) => (
+					{ledLessons.map((lesson) => (
 						<div
-							key={groupLessons[0].lessonUrl}
-							className="relative"
+							key={lesson.lessonUrl}
+							className="relative mr-56"
 							style={{
-								height: setLessonVerticalPosition(
-									groupLessons.length > 1 ? 9 : groupLessons[0].verticalPosition
-								),
-								marginRight: "200px"
+								height: setLessonVerticalPosition(lesson.verticalPosition)
 							}}
 						>
-							{groupLessons.map((lesson) => (
-								<div
-									key={lesson.lessonUrl}
-									className="absolute"
-									style={{ top: setLessonVerticalPosition(lesson.verticalPosition)} }
-								>
-									<Lilypad lesson={lesson} />
-								</div>
-							))}
+							<div
+								className="absolute"
+								style={{ top: setLessonVerticalPosition(lesson.verticalPosition)} }
+							>
+								<Lilypad lesson={lesson} />
+							</div>
 						</div>
 					))}
 				</div>
