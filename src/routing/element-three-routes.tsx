bde74e42feccd023fdae362b23204cb59beb3592
atamayev <75034340+actamayev@users.noreply.github.com>
2025-02-25 12:57:51 -0500

    Package updates, cleanup

--- src/routing/element-three-routes.tsx
+++ src/routing/element-three-routes.tsx
@@ -1,9 +1,9 @@
-import LineFollowingCode from "../components/lab/element-3/line-following/line-following-code"
-import LineFollowingVideo from "../components/lab/element-3/line-following/line-following-video"
-import LineFollowingReading from "../components/lab/element-3/line-following/line-following-reading"
-import InvertedPendulumCode from "../components/lab/element-3/inverted-pendulum/inverted-pendulum-code"
-import InvertedPendulumVideo from "../components/lab/element-3/inverted-pendulum/inverted-pendulum-video"
-import InvertedPendulumReading from "../components/lab/element-3/inverted-pendulum/inverted-pendulum-reading"
+// import LineFollowingCode from "../components/lab/element-3/line-following/line-following-code"
+// import LineFollowingVideo from "../components/lab/element-3/line-following/line-following-video"
+// import LineFollowingReading from "../components/lab/element-3/line-following/line-following-reading"
+// import InvertedPendulumCode from "../components/lab/element-3/inverted-pendulum/inverted-pendulum-code"
+// import InvertedPendulumVideo from "../components/lab/element-3/inverted-pendulum/inverted-pendulum-video"
+// import InvertedPendulumReading from "../components/lab/element-3/inverted-pendulum/inverted-pendulum-reading"
 
 const element3Routes: ElementRoutes[] = [
 	// {
