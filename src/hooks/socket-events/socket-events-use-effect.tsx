e1e58d7097ea0987c1041bcc249b2cd72d643897
Blue Dot Robots <bluedotrobots@gmail.com>
2025-03-03 11:21:48 -0500

    Encoder data (#97) (#98)
    * Log encoder data
    
    * Add IR sensor data
    
    * Show pip color on screen
    
    ---------
    
    Co-authored-by: atamayev <75034340+actamayev@users.noreply.github.com>

--- src/hooks/socket-events/socket-events-use-effect.tsx
+++ src/hooks/socket-events/socket-events-use-effect.tsx
@@ -3,11 +3,13 @@ import isNull from "lodash-es/isNull"
 import { useSocketContext } from "../../contexts/socket-context"
 import useHandlePipStatusUpdate from "./handle-pip-status-update"
 import useHandleMotorControlAck from "./handle-motor-control-ack"
+import useHandleIncomingSensorData from "./handle-incoming-sensor-data"
 
 export default function useSocketEventsUseEffect(): void {
 	const socketClass = useSocketContext()
 	const handlePipStatusUpdate = useHandlePipStatusUpdate()
 	const handleMotorControlAck = useHandleMotorControlAck()
+	const handleIncomingSensorData = useHandleIncomingSensorData()
 
 	useEffect(() => {
 		if (isNull(socketClass.accessToken)) return
@@ -15,10 +17,12 @@ export default function useSocketEventsUseEffect(): void {
 		// Listen for the 'pipStatusUpdate' event emitted from SocketClass
 		socketClass.on("pipStatusUpdate", handlePipStatusUpdate)
 		socketClass.on("motorControlAck", handleMotorControlAck)
+		socketClass.on("incomingSensorData", handleIncomingSensorData)
 
 		return (): void => {
 			socketClass.off("pipStatusUpdate", handlePipStatusUpdate) // Remove listener when the component unmounts
 			socketClass.off("motorControlAck", handleMotorControlAck) // Remove listener when the component unmounts
+			socketClass.off("incomingSensorData", handleIncomingSensorData)
 		}
-	}, [handlePipStatusUpdate, handleMotorControlAck, socketClass])
+	}, [handlePipStatusUpdate, handleMotorControlAck, socketClass, handleIncomingSensorData])
 }
