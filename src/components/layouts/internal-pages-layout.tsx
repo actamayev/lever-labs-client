170cdc51d8b25dbaa8dff41c057a08fe95774532
atamayev <75034340+actamayev@users.noreply.github.com>
2025-03-07 14:48:46 -0500

    Sidebar styles

--- src/components/layouts/internal-pages-layout.tsx
+++ src/components/layouts/internal-pages-layout.tsx
@@ -24,7 +24,7 @@ export default function InternalPagesLayout({ children } : { children: React.Rea
 					>
 						<SidebarTrigger
 							className={cn("-ml-1 w-12 h-12 transition-none rounded-xl",
-								"hover:bg-lightBackgroundHover hover:dark:bg-darkBackgroundHover"
+								"hover:bg-sidebarButtonHoverLight hover:dark:bg-sidebarButtonHoverDark"
 							)}
 						/>
 					</header>
