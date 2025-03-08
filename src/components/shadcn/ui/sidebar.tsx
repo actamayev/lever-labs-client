b015fa125d80d37a56bbe155e4a4757398447001
atamayev <75034340+actamayev@users.noreply.github.com>
2025-03-08 16:18:06 -0500

    Update sidebar.tsx

--- src/components/shadcn/ui/sidebar.tsx
+++ src/components/shadcn/ui/sidebar.tsx
@@ -558,9 +558,9 @@ const sidebarMenuButtonVariants = cva(
   {
     variants: {
       variant: {
-        default: "hover:bg-sidebar-accent hover:text-sidebar-accent-foreground active:bg-sidebarButtonHoverLight", // Add active state for light mode
+        default: "hover:bg-sidebar-accent hover:text-sidebar-accent-foreground active:bg-sidebarButtonHover", // Add active state for light mode
         outline:
-          "bg-background shadow-[0_0_0_1px_hsl(var(--sidebar-border))] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground hover:shadow-[0_0_0_1px_hsl(var(--sidebar-accent))] active:bg-sidebarButtonHoverLight", // Add active state for light mode
+          "bg-background shadow-[0_0_0_1px_hsl(var(--sidebar-border))] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground hover:shadow-[0_0_0_1px_hsl(var(--sidebar-accent))] active:bg-sidebarButtonHover", // Add active state for light mode
       },
       size: {
         default: "h-8 text-sm",
