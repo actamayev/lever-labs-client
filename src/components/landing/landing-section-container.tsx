a92d55eb77b4fa961b3fac904fcea5efe44a876e
atamayev <75034340+actamayev@users.noreply.github.com>
2025-03-12 17:38:07 -0400

    Update landing-section-container.tsx

--- src/components/landing/landing-section-container.tsx
+++ src/components/landing/landing-section-container.tsx
@@ -3,7 +3,7 @@ import { cn } from "../../lib/shadcn/utils"
 export default function LandingSectionContainer({ children, extraClasses = "" } : { children: React.ReactNode, extraClasses?: string }) {
 	return (
 		<div className="relative">
-			<div className={cn("px-20 sm:px-28 md:px-40 lg:px-60 mt-16 sm:mt-24 md:mt-36", extraClasses)}>
+			<div className={cn("px-20 sm:px-28 md:px-40 lg:px-64 mt-16 sm:mt-24 md:mt-36", extraClasses)}>
 				{children}
 			</div>
 		</div>
