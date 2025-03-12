3e541056c52154e1f75c44b293ed6985a6f29583
atamayev <75034340+actamayev@users.noreply.github.com>
2025-03-12 13:38:13 -0400

    Update screen-card.tsx

--- src/components/landing/sensors/screen-card.tsx
+++ src/components/landing/sensors/screen-card.tsx
@@ -1,15 +1,42 @@
+import { useState } from "react"
+import { TvMinimal } from "lucide-react"
 import SensorsSkeleton from "./sensors-skeleton"
 import { bentoIconSize } from "../../../utils/constants"
-import { TvMinimal } from "lucide-react"
 
 export default function ScreenCard() {
+	const [isHovered, setIsHovered] = useState(false)
+
+	const ScreenIcon = () => {
+		return (
+			<div
+				className="relative"
+				onMouseEnter={() => setIsHovered(true)}
+				onMouseLeave={() => setIsHovered(false)}
+			>
+				<TvMinimal size={bentoIconSize} />
+
+				{isHovered && (
+					<div
+						className="absolute bg-blue-500 rounded-full transition-all duration-300"
+						style={{
+							width: "15px",
+							height: "15px",
+							top: "41	%",
+							left: "50%",
+							transform: "translate(-50%, -50%)",
+						}}
+					/>
+				)}
+			</div>
+		)
+	}
+
 	return (
 		<SensorsSkeleton
 			title="OLED Screen"
 			description="Display for text and graphics"
-			icon={<TvMinimal size={bentoIconSize}/>}
+			icon={<ScreenIcon />}
 			outerDivStyles="col-span-1 lg:col-span-1 lg:col-start-3 lg:row-start-4"
-			// paragraphStyles="whitespace-normal lg:whitespace-nowrap"
 		/>
 	)
 }
