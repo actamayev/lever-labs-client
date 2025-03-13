c08545971a772744d0eda03665caf18ba7def161
atamayev <75034340+actamayev@users.noreply.github.com>
2025-03-13 13:15:27 -0400

    Sensors

--- src/components/support/support-header.tsx
+++ src/components/support/support-header.tsx
@@ -1,42 +1,42 @@
-import { useCallback } from "react"
 import toUpper from "lodash-es/toUpper"
 import { Link, useLocation } from "react-router"
 import { cn } from "../../lib/shadcn/utils"
 
 function SupportLink({ page } : { page: "mission" | "contact" }) {
 	const location = useLocation()
-
-	const isActive = useCallback((route: PageNames) => {
-		return location.pathname === route
-	}, [location.pathname])
+	const active = location.pathname === `/${page}`
 
 	return (
-		<li>
+		<li className="relative flex flex-col items-center group">
 			<Link
 				to={`/${page}`}
 				className={cn(
-					"text-gray-800 dark:text-gray-200 hover:!text-pipThemeText duration-0",
-					isActive(`/${page}`) ? "!text-pipThemeText" : ""
+					"text-disabledLilypadIcon hover:!text-pipThemeText duration-0 text-base px-4 py-2 flex flex-col items-center",
+					active ? "!text-pipThemeText" : ""
 				)}
 			>
-				{toUpper(page)}
+				<span>{toUpper(page)}</span>
+				<div
+					className={cn(
+						"absolute -bottom-0.5 w-full h-1 duration-0 cursor-pointer",
+						active ? "bg-pipThemeText" : "group-hover:bg-pipThemeText"
+					)}
+				/>
 			</Link>
 		</li>
 	)
 }
 
 export default function SupportHeader () {
-
-
 	return (
-		<header className="w-full py-6">
+		<header className="w-1/2 py-6">
 			<nav className="container">
-				<ul className="flex justify-start items-center space-x-12 text-lg font-medium">
+				<ul className="flex justify-start items-center space-x-0 text-lg font-medium">
 					<SupportLink page="mission" />
 					<SupportLink page="contact" />
 				</ul>
 			</nav>
-			<div className="container mx-auto mt-4">
+			<div className="container mx-auto">
 				<div className="border-b-2 border-gray-200 dark:border-gray-700 rounded-xl"></div>
 			</div>
 		</header>
--- src/components/support/support-section-container.tsx
+++ src/components/support/support-section-container.tsx
@@ -3,7 +3,7 @@ import { cn } from "../../lib/shadcn/utils"
 export default function SupportSectionContainer({ children } : { children: React.ReactNode }) {
 	return (
 		<div className="relative">
-			<div className={cn("px-4 sm:px-8 md:px-16 lg:px-60 mt-12")}>
+			<div className={cn("px-8 sm:px-8 md:px-16 lg:px-72 mt-12")}>
 				<div className="font-medium text-3xl">
 					About Us
 				</div>
