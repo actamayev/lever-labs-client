4f4097ff3b71629091bbb9e30da70f5535194918
atamayev <75034340+actamayev@users.noreply.github.com>
2025-03-13 14:44:42 -0400

    Minor changes

--- src/components/social-links/youtube-link.tsx
+++ src/components/social-links/youtube-link.tsx
@@ -5,7 +5,7 @@ export default function YouTubeLink() {
 		<a
 			href="https://www.youtube.com/@BlueDotRobots"
 			aria-label="YouTube"
-			className="text-gray-800 hover:text-gray-950 dark:text-gray-200 dark:hover:text-white transition-all duration-300"
+			className="text-gray-800 hover:text-gray-950 dark:text-gray-200 dark:hover:text-white duration-0"
 			target="_blank"
 			rel="noopener noreferrer"
 		>
