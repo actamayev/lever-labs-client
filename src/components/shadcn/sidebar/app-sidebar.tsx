1f30af66cd514b6adf5616c2babc2db0ec665bb1
atamayev <75034340+actamayev@users.noreply.github.com>
2025-03-14 16:54:11 -0400

    Cleanup

--- src/components/shadcn/sidebar/app-sidebar.tsx
+++ src/components/shadcn/sidebar/app-sidebar.tsx
@@ -1,6 +1,5 @@
 import { useLocation } from "react-router"
 import { Sidebar } from "@/components/shadcn/ui/sidebar"
-import { cn } from "../../../lib/shadcn/utils"
 import SandboxSidebar from "./sandbox-sidebar"
 import PrimarySidebar from "./primary/primary-sidebar"
 import { showPrimarySidebarPages } from "../../../utils/constants"
@@ -8,22 +7,17 @@ import { showPrimarySidebarPages } from "../../../utils/constants"
 export default function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 	const location = useLocation()
 	const shouldShowSidebar = showPrimarySidebarPages.includes(location.pathname as PageNames)
-	const isInSandbox = location.pathname.startsWith("/sandbox")
 
 	if (!shouldShowSidebar) return null
 
 	return (
 		<Sidebar
 			collapsible="icon"
-			className={cn(
-				"overflow-hidden [&>[data-sidebar=sidebar]]:flex-row border-disabledLilypadBackground",
-				// Add class to help maintain button styles when sandbox is open
-				isInSandbox && "has-sandbox-sidebar"
-			)}
+			className="overflow-hidden [&>[data-sidebar=sidebar]]:flex-row border-disabledLilypadBackground"
 			{...props}
 		>
 			<PrimarySidebar />
-			{isInSandbox && <SandboxSidebar />}
+			{location.pathname.startsWith("/sandbox") && <SandboxSidebar />}
 		</Sidebar>
 	)
 }
--- src/components/shadcn/sidebar/primary/mapped-nav-data.tsx
+++ src/components/shadcn/sidebar/primary/mapped-nav-data.tsx
@@ -52,7 +52,7 @@ export default function MappedNavData() {
 										"group-data-[collapsible=icon]:!h-[55px] group-data-[collapsible=icon]:!w-[55px]"
 									)}
 								>
-									<div className="flexitems-center justify-center">
+									<div className="flex items-center justify-center">
 										<item.icon
 											className={cn(
 												"!h-[45px] !w-[45px] transition-none",
