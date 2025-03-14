be94b0a691122dd004d7604e4366efee3629ec6d
atamayev <75034340+actamayev@users.noreply.github.com>
2025-03-14 16:48:35 -0400

    Undoing sidebar changes

--- src/components/shadcn/sidebar/primary/primary-sidebar.tsx
+++ src/components/shadcn/sidebar/primary/primary-sidebar.tsx
@@ -1,18 +1,22 @@
-import { Sidebar } from "@/components/shadcn/ui/sidebar"
+import { Sidebar, SidebarFooter } from "@/components/shadcn/ui/sidebar"
 import SidebarLogo from "./sidebar-logo"
+import NavUser from "./nav-user/nav-user"
 import MappedNavData from "./mapped-nav-data"
-import SidebarFooterSection from "./sidebar-footer-section"
+import AddPipSidebarButton from "./add-pip/add-pip-sidebar-button"
 
 export default function PrimarySidebar() {
 	return (
 		<Sidebar
 			collapsible="none"
 			className="!w-[calc(var(--sidebar-width-icon))] border-r
-			bg-standardBackground border-disabledLilypadBackground"
+			bg-standardBackground border-disabledLilypadBbackground"
 		>
 			<SidebarLogo />
 			<MappedNavData />
-			<SidebarFooterSection />
+			<SidebarFooter>
+				<AddPipSidebarButton />
+				<NavUser />
+			</SidebarFooter>
 		</Sidebar>
 	)
 }
--- src/components/shadcn/sidebar/primary/sidebar-logo.tsx
+++ src/components/shadcn/sidebar/primary/sidebar-logo.tsx
@@ -9,17 +9,17 @@ export default function SidebarLogo() {
 	return (
 		<SidebarHeader>
 			<SidebarMenu>
-				<SidebarMenuItem className="flex justify-start">
+				<SidebarMenuItem className="flex justify-center">
 					<Link
 						to="/lab"
-						className="flex !h-14 !w-14 items-center
+						className="flex !h-16 !w-16 !min-w-[64px] items-center
 						justify-center rounded-lg group-data-[collapsible=icon]:!h-16 group-data-[collapsible=icon]:!w-16"
 					>
-						<div className="flex aspect-square !h-14 !w-14 items-center justify-center">
+						<div className="flex aspect-square !h-16 !w-16 items-center justify-center">
 							<img
 								src="/favicon.svg"
 								alt="Logo"
-								className="!h-14 !w-14"
+								className="!h-16 !w-16"
 								loading="lazy"
 							/>
 						</div>
