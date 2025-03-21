e2f80dbd036663fa2fe2f4481187da85efab96f6
atamayev <bluedotrobots@gmail.com>
2025-03-21 17:14:47 -0400

    Update page.tsx

--- app/career-quest/line-following/page.tsx
+++ app/career-quest/line-following/page.tsx
@@ -1,15 +1,18 @@
-/* eslint-disable max-len */
 import AuthenticatedLayout from "../../../src/components/authenticated-layout"
+import { createMetadata } from "../../../src/utils/helmet-data/create-metadata"
 import CareerActivityTemplate from "../../../src/components/career-quest/career-activity-template"
-// import { createMetadata } from "../../../src/utils/helmet-data/create-metadata"
 
-// export const metadata = createMetadata({
-// 	title: "Line Following",
-// 	// eslint-disable-next-line max-len
-// 	description: "Guide Pip through coding challenges like line-following and maze-solving to discover its purpose in a robotics adventure.",
-// 	path: "/career-quest/line-following",
-// 	keywords: ["robotics adventure", "coding challenges", "purpose discovery"]
-// })
+export const metadata = createMetadata({
+	title: "Line Following",
+	// eslint-disable-next-line max-len
+	description: "Guide Pip through line-following to discover its purpose in a robotics adventure.",
+	path: "/career-quest/line-following",
+	keywords: [
+		"robotics adventure",
+		"coding challenges",
+		"line following robot"
+	]
+})
 
 export default function LineFollowingPage() {
 	return (
