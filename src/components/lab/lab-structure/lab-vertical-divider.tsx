ecc692631b0fbd442a81cb7b5daeddef8eeb02d7
atamayev <bluedotrobots@gmail.com>
2025-03-22 18:15:14 -0400

    Some styles changes

--- src/components/lab/lab-structure/lab-vertical-divider.tsx
+++ src/components/lab/lab-structure/lab-vertical-divider.tsx
@@ -1,5 +1,5 @@
 "use client"
 
 export default function LabVerticalDivider() {
-	return <div className="h-full min-h-[600px] w-1 dark:bg-gray-700 bg-gray-300 rounded-full" />
+	return <div className="h-full min-h-[600px] w-1 bg-swan rounded-full" />
 }
