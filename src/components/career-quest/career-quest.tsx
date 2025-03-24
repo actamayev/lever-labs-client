5ed0783788f9edc3947efa414830264bbb14a80e
atamayev <bluedotrobots@gmail.com>
2025-03-24 13:22:52 -0400

    Update career-quest.tsx

--- src/components/career-quest/career-quest.tsx
+++ src/components/career-quest/career-quest.tsx
@@ -6,18 +6,25 @@ import WorkbenchIcons from "../workbench/workbench-icons"
 
 export default function CareerQuest() {
 	return (
-		<div className="flex flex-row h-screen overflow-y-auto relative w-full space-x-6 px-6 pt-12">
+		<div className="flex flex-row h-screen overflow-y-auto relative w-full space-x-12 px-6 pt-12">
 			{/* Main content area - 2/3 width */}
 			<div className="w-full lg:w-3/5 xl:w-[61.8%]">
 				{careerData.map(singleCareerData => (
-					<div className="mb-6 flex flex-col items-center" key={singleCareerData.careerName}>
+					<div className="mb-12 flex flex-col items-center" key={singleCareerData.careerName}>
 						<SingleCareerCard careerData={singleCareerData} />
 					</div>
 				))}
 			</div>
 
-			<div className="w-full lg:w-2/5 xl:w-[38.2%] mt-8 lg:mt-0 space-y-4">
-				<WorkbenchIcons />
+			{/* Fixed workbench icons */}
+			<div className="hidden lg:block lg:w-2/5 xl:w-[38.2%]">
+				{/* This div takes up space in the layout */}
+				<div className="w-full" style={{ height: "1px" }}></div>
+
+				{/* This is the fixed element */}
+				<div className="fixed top-9" style={{ width: "inherit", maxWidth: "inherit" }}>
+					<WorkbenchIcons />
+				</div>
 			</div>
 		</div>
 	)
