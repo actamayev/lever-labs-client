66fb449ba51a4583dd2c36d699032d35440f132b
atamayev <bluedotrobots@gmail.com>
2025-04-04 13:13:08 -0400

    Update the-profile-page.tsx

--- src/components/profile/the-profile-page.tsx
+++ src/components/profile/the-profile-page.tsx
@@ -5,8 +5,8 @@ import ProfileImage from "./profile-image/profile-image"
 
 export default function TheProfilePage() {
 	return (
-		<div className="container mx-auto py-8 px-4">
-			<h1 className="text-3xl font-bold mb-8">Profile</h1>
+		<div className="container mx-auto py-8 px-40">
+			<h1 className="text-3xl font-bold mb-8 text-eel">Profile</h1>
 			<div className="flex flex-row space-x-10">
 				<ProfileImage />
 				<div>
