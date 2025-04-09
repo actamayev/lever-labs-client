5e0084301386e879d8b7721ac427d70c5e9f07fc
atamayev <bluedotrobots@gmail.com>
2025-04-09 14:52:27 -0400

    Update driving-controls.tsx

--- src/components/garage/driving-and-sounds/driving/driving-controls.tsx
+++ src/components/garage/driving-and-sounds/driving/driving-controls.tsx
@@ -3,9 +3,9 @@
 import { observer } from "mobx-react"
 import { useState, useEffect } from "react"
 import ArrowKeyButton from "./arrow-key-button"
+import DrivingActionButton from "./driving-action-button"
 import AdjustMaxDrivingSpeed from "./adjust-max-driving-speed"
 import useHybridDrivingControls from "../../../../hooks/garage/use-driving-controls"
-import DrivingActionButton from "./driving-action-button"
 
 function DrivingControls() {
 	const { handleButtonDown, handleButtonUp, isButtonPressed } = useHybridDrivingControls()
