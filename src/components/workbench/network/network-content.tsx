eff57184ceedbfebb04784fcc79a1d2e2399b115
Ariel Tamayev <actamayev@gmail.com>
2025-06-04 13:52:40 -0400

    Clear wifi networks (#165)
    * Clear wifi networks
    
    * Update network-content.tsx

--- src/components/workbench/network/network-content.tsx
+++ src/components/workbench/network/network-content.tsx
@@ -5,20 +5,40 @@ import { Button } from "../../shadcn/ui/button"
 import { usePipContext } from "../../../contexts/pip-context"
 import useDisconnectFromPip from "../../../hooks/pip/disconnect-from-pip"
 import useRequestToConnectToPip from "../../../hooks/pip/request-to-connect-to-pip"
+import { useSerialManagerContext } from "../../../contexts/serial-manager-context"
+import { useCallback } from "react"
+import { MessageBuilder } from "@bluedotrobots/common-ts"
 
 function NetworkContent() {
 	const pipClass = usePipContext()
 	const disconnectFromPip = useDisconnectFromPip()
 	const requestToConnectToPip = useRequestToConnectToPip()
+	const serialManager = useSerialManagerContext()
+
+	const clearWiFiCredentials = useCallback(async () => {
+		if (serialManager.connected === false) return
+
+		const message = MessageBuilder.createClearWiFiNetworksMessage()
+		await serialManager.sendBinaryMessage(message)
+	}, [serialManager])
 
 	const selectedPip = pipClass.selectedPip
 	if (isNull(selectedPip)) return null
 	switch (selectedPip.pipConnectionStatus) {
 	case "offline":
 		return (
-			<div className="text-base font-medium">
+			<>
+				<div className="text-base font-medium">
 				Please connect {selectedPip.pipName} to the internet
-			</div>
+				</div>
+				<Button
+					onClick={clearWiFiCredentials}
+					className="rounded-xl bg-eel h-9 mt-2"
+					disabled={!serialManager.connected}
+				>
+					CLEAR WIFI CREDENTIALS
+				</Button>
+			</>
 		)
 	case "online":
 		return (
