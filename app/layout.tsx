d8c43e3264fb0916cbba3c87a5faf0439797bd66
atamayev <bluedotrobots@gmail.com>
2025-06-04 11:49:41 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import { Viewport } from "next"
-import Script from "next/script"
 import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import "../src/styles/index.css"
@@ -17,26 +16,14 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-	console.log("process.env.NODE_ENV:", process.env.NODE_ENV)
 	return (
 		<html lang="en" className={`${lexend.variable}`}>
 			<body>
 				<Providers>
 					{children}
 				</Providers>
-				{process.env.NODE_ENV === "production" && (
-					<>
-						<Analytics />
-						<SpeedInsights />
-						<Script
-							src="https://app.rybbit.io/api/script.js"
-							data-site-id="338"
-							async
-							data-track-query="false" // Enhances privacy by not tracking query parameters
-							data-debounce="300" // Slightly faster response to navigation (default is 500ms)
-						/>
-					</>
-				)}
+				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	)
