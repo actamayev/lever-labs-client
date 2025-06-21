0ea32a95f586203979df9461b7ad6f1a2724bf7c
atamayev <bluedotrobots@gmail.com>
2025-06-21 10:38:06 -0400

    Add Cq data

--- src/components/career/introduction/cq-introduction.tsx
+++ src/components/career/introduction/cq-introduction.tsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/shadcn/utils"
 import SimpleSandbox from "../simple-sandbox/simple-sandbox"
 import careerQuestCode from "../../../utils/constants/career-quest/career-quest-code"
 
+// eslint-disable-next-line max-lines-per-function
 export default function CQIntroduction() {
 	return (
 		<div className="w-full min-h-screen p-4">
@@ -64,19 +65,101 @@ export default function CQIntroduction() {
 			</div>
 
 			<div className="w-2/3 mx-auto mb-8 text-center">
-			Nice! What you just saw was me reading over and following the simple code.
-			The box on the left with the blocks is called the sandbox. Think of this as your kitchen table for preparing the recipe.
-			In the sandbox, you can choose the ingredients you want me to use and the tools I'll need to prepare and process your ingredients.
-			Ingredients fallen into two categories, actuators, and sensors.
-			Actuators are part of me that you can control think motors lights, and the speaker. You already had a chance to use my LEDs.
-			Q: which actuator would we use for obstacle avoidance? (MC- duolingo style)
-			Next sensors sensors allow me to observe the world around me just like you have five senses site hearing touch smell taste. I also have sensors. I have three sensors in motion sensor a colored detection sensor and black-and-white detection sensor.
-			Q: What sensor do you think we'll use for obstacle detection? (MC-duolingo style)
-			In order to make use of these ingredients, we need to process them for example if you're making a tomato cucumber salad, you don't just throw the cucumbers and tomatoes into a bowl. You have to wash them and cut them (prepare and process).
-			In the sandbox, those preparation and process is done by logic blocks logic block, lettuce, bring together information we get from sensors and control or actuators. They are the glue that holds the instructions together.
-			Let's talk about a few logic blocks will be using often.
-			1. Delay block
-			Whenever I come across a delay block in the instructions, I will pause everything I'm doing for the time that's indicated in the delay block run the two examples below to see the delay block in action.
+				Nice! What you just saw was me reading over and following the simple code.
+				The box on the left with the blocks is called the sandbox. Think of this as your kitchen table for preparing the recipe.
+				In the sandbox, you can choose the ingredients you want me to use and the tools I'll need to prepare and process your ingredients.
+				Ingredients fallen into two categories, actuators, and sensors.
+				Actuators are part of me that you can control think motors lights, and the speaker. You already had a chance to use my LEDs.
+				Q: which actuator would we use for obstacle avoidance? (MC- duolingo style)
+				Next sensors sensors allow me to observe the world around me just like you have five senses site hearing touch smell taste. I also have sensors. I have three sensors in motion sensor a colored detection sensor and black-and-white detection sensor.
+				Q: What sensor do you think we'll use for obstacle detection? (MC-duolingo style)
+				In order to make use of these ingredients, we need to process them for example if you're making a tomato cucumber salad, you don't just throw the cucumbers and tomatoes into a bowl. You have to wash them and cut them (prepare and process).
+				In the sandbox, those preparation and process is done by logic blocks logic block, lettuce, bring together information we get from sensors and control or actuators. They are the glue that holds the instructions together.
+				Let's talk about a few logic blocks will be using often.
+				1. Delay block
+				Whenever I come across a delay block in the instructions, I will pause everything I'm doing for the time that's indicated in the delay block run the two examples below to see the delay block in action.
+			</div>
+
+			<div className="w-2/3 mx-auto h-52">
+				<div className="flex gap-4 h-full">
+					{/* SimpleSandbox - Left Half */}
+					<div className="flex-1 flex flex-col">
+						<Suspense fallback={<BlocklyLoadingComponent extraClasses="h-full" />}>
+							<SimpleSandbox
+								blocklyJson={careerQuestCode[1].blocklyJson}
+								cppCode={careerQuestCode[1].cppCode}
+							/>
+						</Suspense>
+					</div>
+
+					{/* Code Display - Right Half */}
+					<div className="flex-1 flex flex-col">
+						<div className="flex-1 border-2 border-swan rounded-lg overflow-hidden">
+							<div className="h-full flex flex-col">
+								{/* Header */}
+								<div className="bg-gray-100 dark:bg-gray-800 px-4 py-2 border-b border-swan">
+									<h3 className="text-sm font-medium text-gray-700 dark:text-gray-300">
+										Generated C++ Code
+									</h3>
+								</div>
+
+								{/* Code Content */}
+								<div className="flex-1 overflow-auto">
+									<pre className={cn(
+										"h-full w-full p-4 text-sm font-mono",
+										"bg-white dark:bg-gray-900",
+										"text-gray-800 dark:text-gray-200",
+										"whitespace-pre-wrap break-words",
+										"resize-none outline-none"
+									)}>
+										{careerQuestCode[1].cppCode}
+									</pre>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="w-2/3 mx-auto h-52">
+				<div className="flex gap-4 h-full">
+					{/* SimpleSandbox - Left Half */}
+					<div className="flex-1 flex flex-col">
+						<Suspense fallback={<BlocklyLoadingComponent extraClasses="h-full" />}>
+							<SimpleSandbox
+								blocklyJson={careerQuestCode[2].blocklyJson}
+								cppCode={careerQuestCode[2].cppCode}
+							/>
+						</Suspense>
+					</div>
+
+					{/* Code Display - Right Half */}
+					<div className="flex-1 flex flex-col">
+						<div className="flex-1 border-2 border-swan rounded-lg overflow-hidden">
+							<div className="h-full flex flex-col">
+								{/* Header */}
+								<div className="bg-gray-100 dark:bg-gray-800 px-4 py-2 border-b border-swan">
+									<h3 className="text-sm font-medium text-gray-700 dark:text-gray-300">
+										Generated C++ Code
+									</h3>
+								</div>
+
+								{/* Code Content */}
+								<div className="flex-1 overflow-auto">
+									<pre className={cn(
+										"h-full w-full p-4 text-sm font-mono",
+										"bg-white dark:bg-gray-900",
+										"text-gray-800 dark:text-gray-200",
+										"whitespace-pre-wrap break-words",
+										"resize-none outline-none"
+									)}>
+										{careerQuestCode[2].cppCode}
+									</pre>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
--- src/utils/constants/career-quest/career-quest-code.ts
+++ src/utils/constants/career-quest/career-quest-code.ts
@@ -18,11 +18,58 @@ const careerQuestCode: CareerQuestCode[] = [
 						"y": -90,
 						"fields": {
 							"esp32_led_control": "WHITE"
+						}
+					}
+				]
+			}
+		},
+		cppCode: "rgbLed.set_led_white();"
+	},
+	{
+		blocklyJson: {
+			"blocks": {
+				"languageVersion": 0,
+				"blocks": [
+					{
+						"type": "esp32_led_control",
+						"id": "P)bh$Rk*Ian[~5p#G}wK",
+						"x": -430,
+						"y": -110,
+						"fields": {
+							"esp32_led_control": "WHITE"
+						},
+						"next": {
+							"block": {
+								"type": "esp32_led_control",
+								"id": "Y[;LU:B~*pff}w4]83Ne",
+								"fields": {
+									"esp32_led_control": "GREEN"
+								}
+							}
+						}
+					}
+				]
+			}
+		},
+		cppCode: "rgbLed.set_led_white();\nrgbLed.set_led_green();"
+	},
+	{
+		blocklyJson: {
+			"blocks": {
+				"languageVersion": 0,
+				"blocks": [
+					{
+						"type": "esp32_led_control",
+						"id": "P)bh$Rk*Ian[~5p#G}wK",
+						"x": -510,
+						"y": -170,
+						"fields": {
+							"esp32_led_control": "WHITE"
 						},
 						"next": {
 							"block": {
 								"type": "esp32_delay",
-								"id": "IGmG@OMvL^oNALZ=R:8=",
+								"id": "}6uEENZW6:]pL_2s,_D[",
 								"fields": {
 									"esp32_delay": 1000
 								},
