a414061018dc914fa45cf78bfacbe0f03759bf6e
atamayev <bluedotrobots@gmail.com>
2025-06-22 11:55:00 -0400

    Workbench

--- src/components/workbench/battery/battery-icon.tsx
+++ src/components/workbench/battery/battery-icon.tsx
@@ -5,6 +5,7 @@ import { BatteryCharging, BatteryFull, BatteryLow, BatteryMedium, BatteryWarning
 import { cn } from "../../../lib/shadcn/utils"
 import workbenchClass from "../../../classes/workbench-class"
 import WorkbenchIconTemplate from "../workbench-icon-template"
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "../../shadcn/ui/hover-card"
 
 function BatteryIcon() {
 	// Determine which speaker icon to show based on volume level
@@ -16,6 +17,12 @@ function BatteryIcon() {
 		return "text-macaw"
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [workbenchClass.isCharging, workbenchClass.batteryPercentage])
+
+	const getTimeText = useMemo(() => {
+		if (workbenchClass.isCharging) return "Estimated time to full charge:"
+		return "Estimated time remaining:"
+	// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [workbenchClass.isCharging])
 
 	function BatteryIconToShow() {
 		const baseClasses = "!h-14 !w-14"
@@ -34,21 +41,47 @@ function BatteryIcon() {
 	}
 
 	return (
-		<WorkbenchIconTemplate
-			id="battery-icon"
-			onMouseEnter={() => workbenchClass.setWorkbenchItemToShow("battery")}
-			extraButtonClasses={
-				workbenchClass.workbenchItemToShow === "battery" ? "border-swan relative" : ""
-			}
-		>
-			{workbenchClass.workbenchItemToShow === "battery" && (
-				<div className="absolute bottom-[-2px] left-[0px] right-[0px] h-[3px] bg-standardBackground z-10"></div>
-			)}
-			<BatteryIconToShow />
-			<span className={cn("text-base font-medium -mt-2 text-center", getColorClass)}>
-				{workbenchClass.batteryPercentage}%
-			</span>
-		</WorkbenchIconTemplate>
+		<HoverCard openDelay={0} closeDelay={100}>
+			<HoverCardTrigger asChild>
+				<div>
+					<WorkbenchIconTemplate
+						id="battery-icon"
+						onMouseEnter={() => {}} // No longer needed!
+						extraButtonClasses="hover:border-swan transition-all duration-200"
+					>
+						<BatteryIconToShow />
+						<span className={cn("text-base font-medium -mt-2 text-center", getColorClass)}>
+							{workbenchClass.batteryPercentage}%
+						</span>
+					</WorkbenchIconTemplate>
+				</div>
+			</HoverCardTrigger>
+
+			<HoverCardContent
+				className={cn(
+					"w-80 p-4 border-2 border-swan rounded-2xl text-eel text-base",
+					"bg-standardBackground shadow-lg",
+					"animate-in fade-in-0 zoom-in-95 duration-200",
+					// Duolingo-style rounded corners and playful styling
+					"rounded-tl-none" // Connect to trigger
+				)}
+				side="bottom"
+				align="start"
+				sideOffset={5}
+			>
+				<div className="space-y-2">
+					<div className="font-medium">Battery Status</div>
+					<div className="text-sm">
+						{getTimeText} 2 hours
+					</div>
+					{workbenchClass.isCharging && (
+						<div className="text-xs text-chargingGreen font-medium">
+							⚡ Charging
+						</div>
+					)}
+				</div>
+			</HoverCardContent>
+		</HoverCard>
 	)
 }
 
