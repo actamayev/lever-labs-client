52dbce8e80191d4926961428b1e2dfb9706dcf88
atamayev <bluedotrobots@gmail.com>
2025-07-07 16:05:35 -0400

    Remove lab

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,8 +3,8 @@ import { createMetadata } from "../../src/utils/helmet-data/create-metadata"
 
 export const metadata = createMetadata({
 	title: "Login",
-	description: "Login to your Blue Dot Robots account to access the Lab, track your progress, \
-	and continue your robotics journey with Pip.",
+	description: "Login to your Blue Dot Robots account to access Career Quest, track your progress, \
+	and continue helping Pip discover his perfect robot career.",
 	path: "/login",
 	keywords: ["login", "educational robotics", "student dashboard"]
 })
