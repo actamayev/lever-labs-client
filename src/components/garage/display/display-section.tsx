0d336d1bbdf93a98dea13df9bc3be45b9236f3a0
atamayev <bluedotrobots@gmail.com>
2025-07-24 16:03:48 -0400

    Cleanup

--- src/components/garage/display/display-section.tsx
+++ src/components/garage/display/display-section.tsx
@@ -1,9 +1,15 @@
 "use client"
 
+import { useState } from "react"
+import DisplayColumn from "./display-column"
+import ControlsColumn from "./controls-column"
 import { WORKBENCH_ROUNDING_RADIUS } from "../../../utils/constants/constants"
-import ParentDisplay from "./parent-display"
+import { DISPLAY_HEIGHT, DISPLAY_WIDTH } from "../../../utils/constants/display-constants"
 
 export default function DisplaySection() {
+	const [pixelBuffer, setPixelBuffer] = useState<boolean[][]>(() =>
+		Array(DISPLAY_HEIGHT).fill(null).map(() => Array(DISPLAY_WIDTH).fill(false))
+	)
 	return (
 		<div
 			className="h-1/3 overflow-hidden border-t border-r border-b"
@@ -12,7 +18,12 @@ export default function DisplaySection() {
 				borderBottomRightRadius: WORKBENCH_ROUNDING_RADIUS
 			}}
 		>
-			<ParentDisplay />
+			<div className="flex items-center justify-center bg-standardBackground text-white">
+				<div className="grid grid-cols-2 items-center">
+					<ControlsColumn setPixelBuffer={setPixelBuffer} />
+					<DisplayColumn pixelBuffer={pixelBuffer}/>
+				</div>
+			</div>
 		</div>
 	)
 }
