df8a0864c78bd67ab88c01b2b893efe950e85ab7
atamayev <bluedotrobots@gmail.com>
2025-07-24 16:02:27 -0400

    Update career-data.ts

--- src/utils/constants/career-quest/career-data.ts
+++ src/utils/constants/career-quest/career-data.ts
@@ -76,7 +76,7 @@ export const careerData: CareerData[] = [
 		lessonsComplete: 0,
 		careerUrl: "/career-quest/obstacle-avoidance",
 		careerIcon: Route,
-		backgroundColor: "beetle",
+		backgroundColor: "macaw",
 		expectedCompletionTime: "1 HOUR",
 		componentsUsed: [
 			{
@@ -109,7 +109,7 @@ export const careerData: CareerData[] = [
 		lessonsComplete: 0,
 		careerUrl: "/career-quest/obstacle-avoidance",
 		careerIcon: Route,
-		backgroundColor: "macaw",
+		backgroundColor: "cardinal",
 		expectedCompletionTime: "1 HOUR",
 		componentsUsed: [
 			{
@@ -175,7 +175,40 @@ export const careerData: CareerData[] = [
 		lessonsComplete: 0,
 		careerUrl: "/career-quest/obstacle-avoidance",
 		careerIcon: Route,
-		backgroundColor: "cardinal",
+		backgroundColor: "beetle",
+		expectedCompletionTime: "1 HOUR",
+		componentsUsed: [
+			{
+				componentName: "Side Distance Sensors",
+				componentDifficulty: 2,
+			},
+			{
+				componentName: "Multizone Distance Sensor",
+				componentDifficulty: 2,
+			},
+			{
+				componentName: "Motors + Encoders",
+				componentDifficulty: 2,
+			},
+			{
+				componentName: "LED",
+				componentDifficulty: 1,
+			}
+		],
+		codingConcepts: [
+			"Boolean Logic",
+			"Conditional Statements",
+			"Loops"
+		]
+	},
+	{
+		careerName: "Obstacle Avoidance",
+		careerDescription: "Program Pip to navigate around obstacles using distance sensors to detect objects in its path. Learn to implement smart navigation algorithms that help Pip explore environments safely and efficiently.",
+		totalLessons: 8,
+		lessonsComplete: 0,
+		careerUrl: "/career-quest/obstacle-avoidance",
+		careerIcon: Route,
+		backgroundColor: "beakInner",
 		expectedCompletionTime: "1 HOUR",
 		componentsUsed: [
 			{
