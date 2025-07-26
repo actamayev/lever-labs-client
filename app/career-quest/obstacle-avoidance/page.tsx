a8b7ca39138f8096241b34622255fde3bc933ffc
atamayev <bluedotrobots@gmail.com>
2025-07-26 11:47:15 -0400

    Update page.tsx

--- app/career-quest/obstacle-avoidance/page.tsx
+++ app/career-quest/obstacle-avoidance/page.tsx
@@ -1,7 +1,6 @@
 import AuthenticatedLayout from "../../../src/components/authenticated-layout"
 import { createMetadata } from "../../../src/utils/helmet-data/create-metadata"
 import CareerActivityTemplate from "../../../src/components/career/career-activity-template"
-import ObstacleAvoidance from "../../../src/components/career/obstacle-avoidance/obstacle-avoidance"
 import NewObstacleAvoidance from "../../../src/components/career/obstacle-avoidance/new-obstacle-avoidance"
 
 export const metadata = createMetadata({
