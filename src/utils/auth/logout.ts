21c65eed07e291d8988d0c3f1260ed809104d77b
atamayev <bluedotrobots@gmail.com>
2025-08-01 13:36:38 -0400

    Update logout.ts

--- src/utils/auth/logout.ts
+++ src/utils/auth/logout.ts
@@ -5,14 +5,14 @@ import authClass from "../../classes/auth-class"
 import socketClass from "../../classes/socket-class"
 import garageClass from "../../classes/garage-class"
 import sandboxClass from "../../classes/sandbox-class"
+import studentClass from "../../classes/student-class"
+import teacherClass from "../../classes/teacher-class"
 import workbenchClass from "../../classes/workbench-class"
 import careerQuestClass from "../../classes/career-quest-class"
 import personalInfoClass from "../../classes/personal-info-class"
+import blueDotApiClientClass from "../../classes/blue-dot-api-client-class"
 import serialMessageManagerClass from "../../classes/serial-message-manager-class"
 import serialConnectionManagerClass from "../../classes/serial-connection-manager-class"
-import studentClass from "../../classes/student-class"
-import teacherClass from "../../classes/teacher-class"
-import blueDotApiClientClass from "../../classes/blue-dot-api-client-class"
 
 export default async function logout(): Promise<void> {
 	try {
