30fa9906b8cdef6ab8941c3e6231fe53d3728b02
atamayev <bluedotrobots@gmail.com>
2025-08-03 22:27:22 -0400

    Cleanup

--- src/hooks/career-quest/use-effect-retrieve-all-careers-challenges.ts
+++ src/hooks/career-quest/use-effect-retrieve-all-careers-challenges.ts
@@ -1,10 +1,11 @@
 import { useEffect } from "react"
+import authClass from "../../classes/auth-class"
 import careerQuestClass from "../../classes/career-quest-class"
 import retrieveCareerQuestChallengeData from "../../utils/career-quest/retrieve-full-career-data"
 
 export default function useEffectRetrieveAllCareersChallenges(): void {
 	useEffect(() => {
-		if (!careerQuestClass.isDoneInitializing) return
+		if (!careerQuestClass.isDoneInitializing || !authClass.isFinishedWithSignup) return
 
 		careerQuestClass.careers.forEach(career => {
 			try {
--- src/hooks/career-quest/use-effect-retrieve-single-career-challenges.ts
+++ src/hooks/career-quest/use-effect-retrieve-single-career-challenges.ts
@@ -2,10 +2,11 @@ import { useEffect } from "react"
 import { CareerUUID } from "@bluedotrobots/common-ts"
 import careerQuestClass from "../../classes/career-quest-class"
 import retrieveCareerQuestChallengeData from "../../utils/career-quest/retrieve-full-career-data"
+import authClass from "../../classes/auth-class"
 
 export default function useEffectRetrieveSingleCareerChallenges(careerUUID: CareerUUID): void {
 	useEffect(() => {
-		if (!careerQuestClass.isDoneInitializing) return
+		if (!careerQuestClass.isDoneInitializing || !authClass.isFinishedWithSignup) return
 
 		try {
 			void retrieveCareerQuestChallengeData(careerUUID)
