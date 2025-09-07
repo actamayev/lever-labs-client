9c314a73a6653ea3460f94d8b32bc23569bdeb12
actamayev <bluedotrobots@gmail.com>
2025-09-06 21:43:45 -0400

    Add speaker blocks

---
+++ src/utils/blockly/custom-blocks/speaker-blocks.ts
@@ -0,0 +1,39 @@
+"use client"
+
+import * as Blockly from "blockly"
+import { speakerCategoryColour } from "../../constants/constants"
+import { SENSOR_TYPES, SPEAKER_BLOCK_TYPES, SoundNameSensorType } from "@bluedotrobots/common-ts"
+import { upperFirst } from "lodash-es"
+
+export const speakerBlocks: Record<SPEAKER_BLOCK_TYPES, CustomBlock> = {
+	[SPEAKER_BLOCK_TYPES.PLAY_SOUND]: {
+		definition: {
+			init: function(this: Blockly.Block): void {
+				this.appendDummyInput()
+					.appendField("Play Sound")
+					.appendField(
+						new Blockly.FieldDropdown(
+							Object.entries(SENSOR_TYPES.SOUND_NAMES).map(([key, value]): [string, string] =>
+								[upperFirst(key.toLowerCase()), value]
+							)
+						),
+						SPEAKER_BLOCK_TYPES.PLAY_SOUND
+					)
+				this.setPreviousStatement(true, null)
+				this.setNextStatement(true, null)
+				this.setColour(speakerCategoryColour)
+				this.setTooltip("Play a sound")
+			},
+			keywords: [
+				"sound", "play", "music", "audio", "beep", "tone", "noise", "speaker", "volume",
+				"chime", "chirp", "pop", "drop", "fart", "monkey", "elephant", "party", "ufo", "countdown", "engine", "robot"
+			]
+		},
+		generator: (block: Blockly.Block): string => {
+			const state = block.getFieldValue(SPEAKER_BLOCK_TYPES.PLAY_SOUND) as SoundNameSensorType
+			// Make the first letter uppercase, rest lowercase
+			const upperFirstState = upperFirst(state.toLowerCase())
+			return `playSound("${upperFirstState}");\n`
+		}
+	}
+}
