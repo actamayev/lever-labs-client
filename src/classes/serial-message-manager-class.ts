f30f43b37bf0e6b9e500358f1a8465d5fa2e1410
actamayev <bluedotrobots@gmail.com>
2025-09-16 20:30:35 -0400

    Forget netowrk

--- src/classes/serial-message-manager-class.ts
+++ src/classes/serial-message-manager-class.ts
@@ -22,6 +22,7 @@ interface MessageSentData {
 class SerialMessageManagerClass {
 	public messages: SerialMessage[] = []
 	public onWiFiConnectionResult: ((status: WiFiConnectionStatus) => void) | null = null
+	public onWiFiDeletionResult: ((success: boolean) => void) | null = null
 
 	// Pip flow state
 	public pipId: PipUUID | null = null
@@ -244,9 +245,13 @@ class SerialMessageManagerClass {
 				break
 			}
 			case "/dino-score": {
-				const dinoScore = message.payload
-				gamesClass.addDinoScore(dinoScore.score)
-				void sendDinoScore(dinoScore.score)
+				gamesClass.addDinoScore(message.payload.score)
+				void sendDinoScore(message.payload.score)
+				break
+			}
+			case "/wifi-deleted-network": {
+				const success = message.payload.status
+				this.onWiFiDeletionResult?.(success)
 				break
 			}
 			default:
@@ -299,6 +304,10 @@ class SerialMessageManagerClass {
 		if (!this.savedNetworks.find((n): boolean => n.ssid === network.ssid)) {
 			this.savedNetworks.push(network)
 		}
+	})
+
+	public removeSavedNetwork = action((ssid: string): void => {
+		this.savedNetworks = this.savedNetworks.filter((network): boolean => network.ssid !== ssid)
 	})
 
 	public logout = action((): void => {
