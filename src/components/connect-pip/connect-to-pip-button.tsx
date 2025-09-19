6d37adadf883b235f6c60e59a4c8600bf7d0a5de
Blue Dot Robots <bluedotrobots@gmail.com>
2025-09-18 23:13:54 -0400

    Staging (#229)
    * Change max driving distance to 20 feet
    
    * Update motors-blocks.ts
    
    * Search for pip when we open dialog
    
    * Error fixes

--- src/components/connect-pip/connect-to-pip-button.tsx
+++ src/components/connect-pip/connect-to-pip-button.tsx
@@ -1,13 +1,14 @@
 "use client"
 
 import { observer } from "mobx-react"
+import { BotIcon } from "lucide-react"
 import { TactileButton } from "../shadcn/ui/tactile-button"
 import ConnectToPipDialog from "./connect-pip-dialog"
 import { DuolingoColorVariants } from "../../utils/get-duolingo-colors"
 import pipClass from "../../classes/pip-class"
-import { BotIcon } from "lucide-react"
-import { WORKBENCH_ROUNDING_RADIUS } from "../../utils/constants/constants"
 import { cn } from "../../lib/shadcn/utils"
+import { WORKBENCH_ROUNDING_RADIUS } from "../../utils/constants/constants"
+import searchPipByUUIDUtil from "../../utils/pip/search-pip-by-uuid-util"
 
 interface ConnectToPipButtonProps {
 	colors: DuolingoColorVariants
@@ -18,10 +19,18 @@ interface ConnectToPipButtonProps {
 function ConnectToPipButton(props: ConnectToPipButtonProps): React.ReactNode {
 	const { colors, tactileButtonClasses, botIconClasses } = props
 
+	const handleClick = async (): Promise<void> => {
+		pipClass.setIsConnectPipDialogOpen(true)
+
+		// If there's already a valid 5-character pipUUID, trigger the search
+		if (pipClass.pipUUIDSearchTerm && pipClass.pipUUIDSearchTerm.length === 5) {
+			await searchPipByUUIDUtil(pipClass.pipUUIDSearchTerm)
+		}
+	}
 	return (
 		<>
 			<TactileButton
-				onClick={(): void => pipClass.setIsConnectPipDialogOpen(true)}
+				onClick={handleClick}
 				className={cn("w-full h-full text-white font-semibold", colors.bg, tactileButtonClasses)}
 				shadowHeight={4}
 				shadowClass={colors.shadow2}
--- src/utils/pip/search-for-pip-by-uuid.ts
+++ src/utils/pip/search-for-pip-by-uuid.ts
@@ -1,20 +1,22 @@
 import isEqual from "lodash-es/isEqual"
 import { PipUUID } from "@bluedotrobots/common-ts/types/utils"
-import { isErrorResponses, isMessageResponse } from "../type-checks"
+import { isMessageResponse, isNonSuccessResponse } from "../type-checks"
 import blueDotApiClient from "../../classes/blue-dot-api-client-class"
 import { RetrieveIsPipUUIDValidResponse } from "@bluedotrobots/common-ts/types/api"
+import { AxiosError } from "axios"
 
 export default async function searchForPipByUUID(pipUUID: PipUUID): Promise<RetrieveIsPipUUIDValidResponse | string> {
 	try {
 		const response = await blueDotApiClient.pipDataService.retrievePipUUIDStatus(pipUUID)
-		if (!isEqual(response.status, 200) || isErrorResponses(response.data)) {
+		if (!isEqual(response.status, 200) || isNonSuccessResponse(response.data)) {
 			throw new Error("Search for Pip by UUID failed")
-		}
-		if (isMessageResponse(response.data)) {
-			return response.data.message
 		}
 		return response.data
 	} catch (error) {
+		console.error(error)
+		if (error instanceof AxiosError && isMessageResponse(error.response?.data)) {
+			return error.response?.data.message || "Please try again"
+		}
 		console.error(error)
 		return "Please try again"
 	}
