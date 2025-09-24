9170cfd246a5fff91a92cf8f382d9d0774569f3a
actamayev <bluedotrobots@gmail.com>
2025-09-24 18:54:09 -0400

    Conditinoally update status

--- src/utils/socket/handle-pip-status-update.tsx
+++ src/utils/socket/handle-pip-status-update.tsx
@@ -4,20 +4,29 @@ import { PipConnectionUpdate } from "@bluedotrobots/common-ts/types/socket"
 import pipClass from "../../classes/pip-class"
 import toastClass from "../../classes/toast-class"
 import workbenchClass from "../../classes/workbench-class"
+import serialConnectionManagerClass from "../../classes/serial-connection-manager-class"
 
 export default function handlePipStatusUpdate(data: PipConnectionUpdate): void {
+	console.log("handlePipStatusUpdate", data)
+	const { newConnectionStatus, pipUUID } = data
+
+	// If we're getting an "offline" status but we have an active serial connection, ignore it
+	if (newConnectionStatus === "offline" &&
+        serialConnectionManagerClass.pipTurnedOn &&
+        pipClass.selectedPip?.pipUUID === pipUUID) {
+		console.log("Ignoring offline status - serial connection is active")
+		return
+	}
+
 	pipClass.updatePipConnectionStatus(data)
-	const { newConnectionStatus } = data
+
 	switch (newConnectionStatus) {
 		case "offline": {
-			if (pipClass.selectedPip?.pipConnectionStatus !== "connected to serial to you") {
-				workbenchClass.setBatteryDataNull()
-				pipClass.deletePip()
-				return toastClass.neutral({
-					title: "Your Pip has disconnected from the internet"
-				})
-			}
-			break
+			workbenchClass.setBatteryDataNull()
+			pipClass.deletePip()
+			return toastClass.neutral({
+				title: "Your Pip has disconnected from the internet"
+			})
 		}
 	}
 }
