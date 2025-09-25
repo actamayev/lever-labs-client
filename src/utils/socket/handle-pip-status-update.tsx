803f6e2a2c1df85ceb491c390d31b294458b7457
actamayev <bluedotrobots@gmail.com>
2025-09-25 16:30:59 -0400

    Update handle-pip-status-update.tsx

--- src/utils/socket/handle-pip-status-update.tsx
+++ src/utils/socket/handle-pip-status-update.tsx
@@ -7,14 +7,12 @@ import workbenchClass from "../../classes/workbench-class"
 import serialConnectionManagerClass from "../../classes/serial-connection-manager-class"
 
 export default function handlePipStatusUpdate(data: PipConnectionUpdate): void {
-	console.log("handlePipStatusUpdate", data)
 	const { newConnectionStatus, pipUUID } = data
 
 	// If we're getting an "offline" status but we have an active serial connection, ignore it
 	if (newConnectionStatus === "offline" &&
         serialConnectionManagerClass.pipTurnedOn &&
         pipClass.selectedPip?.pipUUID === pipUUID) {
-		console.log("Ignoring offline status - serial connection is active")
 		return
 	}
 
