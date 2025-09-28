8d984f2adf2ba1d46565cfe7d315f2f1e0a6eeb1
actamayev <bluedotrobots@gmail.com>
2025-09-27 20:24:46 -0400

    Social links

--- src/components/social-links/youtube-link.tsx
+++ src/components/social-links/youtube-link.tsx
@@ -5,7 +5,7 @@ import { CustomYoutube } from "../../icons/custom-youtube"
 export default function YouTubeLink(): React.ReactNode {
 	return (
 		<a
-			href="https://www.youtube.com/@BlueDotRobots"
+			href="https://www.youtube.com/@Lever_Labs"
 			aria-label="YouTube"
 			className="text-questionText hover:text-gray-950 dark:hover:text-white duration-0"
 			target="_blank"
