c73abbe545d56062d860fd92df45b593ba34598c
Ariel Tamayev <actamayev@gmail.com>
2025-09-30 17:14:38 -0400

    Staging (#238)
    * Update use-initialize-google-analytics.ts
    
    * Update use-initialize-google-analytics.ts

--- src/hooks/analytics/use-initialize-google-analytics.ts
+++ src/hooks/analytics/use-initialize-google-analytics.ts
@@ -12,7 +12,7 @@ export default function useInitializeGoogleAnalytics(): void {
 		const initializeGA = async (): Promise<void> => {
 			try {
 				console.log("process.env.VERCEL_ENV", process.env.VERCEL_ENV)
-				if (process.env.VERCEL_ENV !== "production") return
+				console.log("process.env.NODE_ENV", process.env.NODE_ENV)
 				// Dynamically import ReactGA only when needed
 				const ReactGA = (await import("react-ga4")).default
 				ReactGA.initialize(process.env.NEXT_PUBLIC_MEASUREMENT_ID as string)
@@ -32,7 +32,7 @@ export default function useInitializeGoogleAnalytics(): void {
 
 		const sendPageView = async (): Promise<void> => {
 			console.log("process.env.VERCEL_ENV", process.env.VERCEL_ENV)
-			if (process.env.VERCEL_ENV !== "production") return
+			console.log("process.env.NODE_ENV", process.env.NODE_ENV)
 			const ReactGA = (await import("react-ga4")).default
 			ReactGA.send({
 				hitType: "pageview",
