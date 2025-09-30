58673c77a0e8fec43cb18bbe6cd5889906c2b673
lever-labs <hello@leverlabs.com>
2025-09-30 17:14:26 -0400

    Update use-initialize-google-analytics.ts

--- src/hooks/analytics/use-initialize-google-analytics.ts
+++ src/hooks/analytics/use-initialize-google-analytics.ts
@@ -12,6 +12,7 @@ export default function useInitializeGoogleAnalytics(): void {
 		const initializeGA = async (): Promise<void> => {
 			try {
 				console.log("process.env.VERCEL_ENV", process.env.VERCEL_ENV)
+				console.log("process.env.NODE_ENV", process.env.NODE_ENV)
 				// Dynamically import ReactGA only when needed
 				const ReactGA = (await import("react-ga4")).default
 				ReactGA.initialize(process.env.NEXT_PUBLIC_MEASUREMENT_ID as string)
@@ -31,6 +32,7 @@ export default function useInitializeGoogleAnalytics(): void {
 
 		const sendPageView = async (): Promise<void> => {
 			console.log("process.env.VERCEL_ENV", process.env.VERCEL_ENV)
+			console.log("process.env.NODE_ENV", process.env.NODE_ENV)
 			const ReactGA = (await import("react-ga4")).default
 			ReactGA.send({
 				hitType: "pageview",
