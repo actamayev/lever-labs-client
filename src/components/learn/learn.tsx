0f295182fb30e243ac139d9e87322beb58afd1a4
lever-labs <hello@leverlabs.com>
2025-10-03 06:45:24 -0400

    Cleanup

--- src/components/learn/learn.tsx
+++ src/components/learn/learn.tsx
@@ -12,7 +12,7 @@ function Learn(): React.ReactNode {
 	}, [])
 
 	return (
-		<WorkbenchLayout preventElasticScroll={true}>
+		<WorkbenchLayout preventElasticScroll={true} extraChildrenClasses="p-10">
 			<div className="container mx-auto">
 				<div className="mb-8">
 					<h1 className="text-3xl font-bold text-gray-900 dark:text-white mb-2">
