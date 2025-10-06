5d622b2a7eb0b06fc82c670db96ed119018c434a
lever-labs <hello@leverlabs.com>
2025-10-06 09:36:44 -0400

    Update sound-manager-class.ts

--- src/classes/utility/sound-manager-class.ts
+++ src/classes/utility/sound-manager-class.ts
@@ -1,6 +1,7 @@
 class SoundManager {
 	private correctSound: HTMLAudioElement | null = null
 	private wrongSound: HTMLAudioElement | null = null
+	private levelPassedSound: HTMLAudioElement | null = null
 	private initialized = false
 
 	public initialize(): void {
@@ -8,11 +9,12 @@ class SoundManager {
 
 		this.correctSound = new Audio("/sounds/correct.mp3")
 		this.wrongSound = new Audio("/sounds/wrong.mp3")
+		this.levelPassedSound = new Audio("/sounds/level-passed.mp3")
 
 		// Preload the audio
 		this.correctSound.preload = "auto"
 		this.wrongSound.preload = "auto"
-
+		this.levelPassedSound.preload = "auto"
 		this.initialized = true
 	}
 
@@ -24,10 +26,15 @@ class SoundManager {
 		this.wrongSound?.play()
 	}
 
+	public playLevelPassed(): void {
+		this.levelPassedSound?.play()
+	}
+
 	public cleanup(): void {
 		this.correctSound = null
 		this.wrongSound = null
 		this.initialized = false
+		this.levelPassedSound = null
 	}
 }
 
