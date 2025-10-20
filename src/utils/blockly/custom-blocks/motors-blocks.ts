9102b7b62193f2233aedecf836264b78c3985de4
lever-labs <hello@leverlabs.com>
2025-10-20 15:44:26 -0400

    Bidirectional drive block

--- src/utils/blockly/custom-blocks/motors-blocks.ts
+++ src/utils/blockly/custom-blocks/motors-blocks.ts
@@ -1,16 +1,26 @@
 "use client"
 
 import * as Blockly from "blockly"
+import { upperFirst, toUpper } from "lodash-es"
+import { MOTOR_BLOCK_TYPES, MOTOR_FIELD_VALUES, TURN_DIRECTIONS, MOTOR_DIRECTIONS } from "@lever-labs/common-ts/types/blockly/motor"
 import { motorsCategoryColour } from "../../constants/constants"
-import { MOTOR_BLOCK_TYPES, MOTOR_FIELD_VALUES, TURN_DIRECTIONS } from "@lever-labs/common-ts/types/blockly/motor"
-import { upperFirst } from "lodash-es"
 
 export const motorsBlocks: Record<MOTOR_BLOCK_TYPES, CustomBlock> = {
-	[MOTOR_BLOCK_TYPES.GO_FORWARD]: {
+	[MOTOR_BLOCK_TYPES.DRIVE]: {
 		definition: {
 			init: function(this: Blockly.Block): void {
 				this.appendDummyInput()
-					.appendField("Go forward at")
+					.appendField("Drive")
+					.appendField(
+						new Blockly.FieldDropdown([
+							[upperFirst(MOTOR_DIRECTIONS.FORWARD.toLowerCase()), MOTOR_DIRECTIONS.FORWARD],
+							[upperFirst(MOTOR_DIRECTIONS.BACKWARD.toLowerCase()), MOTOR_DIRECTIONS.BACKWARD]
+						].map(([key, value]): [string, string] =>
+							[upperFirst(key.toLowerCase()), value]
+						)),
+						MOTOR_FIELD_VALUES.DIRECTION
+					)
+					.appendField("at")
 
 				// Use a number field with min and max constraints
 				const percentField = new Blockly.FieldNumber(20, 0, 100, 1)
@@ -24,43 +34,30 @@ export const motorsBlocks: Record<MOTOR_BLOCK_TYPES, CustomBlock> = {
 				this.setTooltip("Move Pip  forward at specified percentage (0-100%)")
 				this.setInputsInline(true)
 			},
-			keywords: ["motor", "move", "advance", "drive", "straight", "ahead", "fast", "slow", "speed", "velocity"]
+			keywords: ["motor", "move", "advance", "drive", "straight", "ahead",
+				"fast", "slow", "speed", "velocity", "forward", "backward", "reverse", "return", "undo", "rewind"]
 		},
 		generator: (block: Blockly.Block): string => {
+			const direction = block.getFieldValue(MOTOR_FIELD_VALUES.DIRECTION) || MOTOR_DIRECTIONS.FORWARD
 			const percentage = block.getFieldValue(MOTOR_FIELD_VALUES.DRIVING_PERCENTAGE) || "0"
-			return `goForward(${percentage});\n`
+			return `drive(${toUpper(direction)}, ${percentage});\n`
 		}
 	},
-	[MOTOR_BLOCK_TYPES.GO_BACKWARD]: {
+	[MOTOR_BLOCK_TYPES.DRIVE_TIME]: {
 		definition: {
 			init: function(this: Blockly.Block): void {
 				this.appendDummyInput()
-					.appendField("Go backward at")
-
-				// Use a number field with min and max constraints
-				const percentField = new Blockly.FieldNumber(20, 0, 100, 1)
-				this.appendDummyInput()
-					.appendField(percentField, MOTOR_FIELD_VALUES.DRIVING_PERCENTAGE)
-					.appendField("% speed")
-
-				this.setPreviousStatement(true, null)
-				this.setNextStatement(true, null)
-				this.setColour(motorsCategoryColour)
-				this.setTooltip("Move Pip  backward at specified percentage (0-100%)")
-				this.setInputsInline(true)
-			},
-			keywords: ["motor", "reverse", "back", "retreat", "return", "undo", "rewind", "speed", "velocity"]
-		},
-		generator: (block: Blockly.Block): string => {
-			const percentage = block.getFieldValue(MOTOR_FIELD_VALUES.DRIVING_PERCENTAGE) || "0"
-			return `goBackward(${percentage});\n`
-		}
-	},
-	[MOTOR_BLOCK_TYPES.GO_FORWARD_TIME]: {
-		definition: {
-			init: function(this: Blockly.Block): void {
-				this.appendDummyInput()
-					.appendField("Go forward for")
+					.appendField("Drive")
+					.appendField(
+						new Blockly.FieldDropdown([
+							[upperFirst(MOTOR_DIRECTIONS.FORWARD.toLowerCase()), MOTOR_DIRECTIONS.FORWARD],
+							[upperFirst(MOTOR_DIRECTIONS.BACKWARD.toLowerCase()), MOTOR_DIRECTIONS.BACKWARD]
+						].map(([key, value]): [string, string] =>
+							[upperFirst(key.toLowerCase()), value]
+						)),
+						MOTOR_FIELD_VALUES.DIRECTION
+					)
+					.appendField("for")
 
 				// Create the seconds label field first so we can reference it
 				const secondsLabelField = new Blockly.FieldLabelSerializable("seconds")
@@ -97,68 +94,30 @@ export const motorsBlocks: Record<MOTOR_BLOCK_TYPES, CustomBlock> = {
 				// Set initial label
 				updateSecondsLabel(2)
 			},
-			keywords: ["motor", "move", "advance", "drive", "duration", "timer", "temporary", "timed", "seconds", "time"]
+			keywords: ["motor", "move", "advance", "drive", "duration", "timer", "temporary",
+				"timed", "seconds", "time", "forward", "backward", "reverse", "return", "undo", "rewind"]
 		},
 		generator: (block: Blockly.Block): string => {
+			const direction = block.getFieldValue(MOTOR_FIELD_VALUES.DIRECTION) || MOTOR_DIRECTIONS.FORWARD
 			const seconds = block.getFieldValue(MOTOR_FIELD_VALUES.DRIVING_SECONDS) || "0"
 			const percentage = block.getFieldValue(MOTOR_FIELD_VALUES.DRIVING_PERCENTAGE) || "0"
-			return `goForwardTime(${seconds}, ${percentage});\n`
+			return `drive_time(${toUpper(direction)}, ${seconds}, ${percentage});\n`
 		}
 	},
-	[MOTOR_BLOCK_TYPES.GO_BACKWARD_TIME]: {
+	[MOTOR_BLOCK_TYPES.DRIVE_DISTANCE]: {
 		definition: {
 			init: function(this: Blockly.Block): void {
 				this.appendDummyInput()
-					.appendField("Go backward for")
-
-				// Create the seconds label field first so we can reference it
-				const secondsLabelField = new Blockly.FieldLabelSerializable("seconds")
-
-				// Add time input
-				const secondsField = new Blockly.FieldNumber(2, 0.1, 10, 0.1)
-				this.appendDummyInput()
-					.appendField(secondsField, MOTOR_FIELD_VALUES.DRIVING_SECONDS)
-					.appendField(secondsLabelField, "SECONDS_LABEL")
-					.appendField("at")
-
-				// Add speed input
-				const percentField = new Blockly.FieldNumber(20, 0, 100, 1)
-				this.appendDummyInput()
-					.appendField(percentField, MOTOR_FIELD_VALUES.DRIVING_PERCENTAGE)
-					.appendField("% speed")
-
-				this.setPreviousStatement(true, null)
-				this.setNextStatement(true, null)
-				this.setColour(motorsCategoryColour)
-				this.setTooltip("Move Pip  backward for specified time and speed")
-				this.setInputsInline(true)
-
-				// Function to update the seconds label
-				const updateSecondsLabel = (newValue: number | string): number | string => {
-					const label = newValue === 1 ? "second" : "seconds"
-					secondsLabelField.setValue(label)
-					return newValue // Return the value unchanged
-				}
-
-				// Set validator on the number field
-				secondsField.setValidator(updateSecondsLabel)
-
-				// Set initial label
-				updateSecondsLabel(2)
-			},
-			keywords: ["motor", "reverse", "back", "retreat", "duration", "timer", "temporary", "timed", "seconds", "time"]
-		},
-		generator: (block: Blockly.Block): string => {
-			const seconds = block.getFieldValue(MOTOR_FIELD_VALUES.DRIVING_SECONDS) || "0"
-			const percentage = block.getFieldValue(MOTOR_FIELD_VALUES.DRIVING_PERCENTAGE) || "0"
-			return `goBackwardTime(${seconds}, ${percentage});\n`
-		}
-	},
-	[MOTOR_BLOCK_TYPES.GO_FORWARD_DISTANCE]: {
-		definition: {
-			init: function(this: Blockly.Block): void {
-				this.appendDummyInput()
-					.appendField("Go forward")
+					.appendField("Drive")
+					.appendField(
+						new Blockly.FieldDropdown([
+							[upperFirst(MOTOR_DIRECTIONS.FORWARD.toLowerCase()), MOTOR_DIRECTIONS.FORWARD],
+							[upperFirst(MOTOR_DIRECTIONS.BACKWARD.toLowerCase()), MOTOR_DIRECTIONS.BACKWARD]
+						].map(([key, value]): [string, string] =>
+							[upperFirst(key.toLowerCase()), value]
+						)),
+						MOTOR_FIELD_VALUES.DIRECTION
+					)
 
 				// Create the inches label field first so we can reference it
 				const inchesLabelField = new Blockly.FieldLabelSerializable("inches")
@@ -195,61 +154,14 @@ export const motorsBlocks: Record<MOTOR_BLOCK_TYPES, CustomBlock> = {
 				// Set initial label
 				updateInchesLabel(10)
 			},
-			keywords: ["motor", "move", "advance", "drive", "distance", "in", "inches", "length", "travel"]
+			keywords: ["motor", "move", "advance", "drive", "distance", "in", "inches",
+				"length", "travel", "forward", "backward", "reverse", "return", "undo", "rewind"]
 		},
 		generator: (block: Blockly.Block): string => {
+			const direction = block.getFieldValue(MOTOR_FIELD_VALUES.DIRECTION) || MOTOR_DIRECTIONS.FORWARD
 			const distance = block.getFieldValue(MOTOR_FIELD_VALUES.DRIVING_DISTANCE) || "0"
 			const percentage = block.getFieldValue(MOTOR_FIELD_VALUES.DRIVING_PERCENTAGE) || "0"
-			return `goForwardDistance(${distance}, ${percentage});\n`
-		}
-	},
-	[MOTOR_BLOCK_TYPES.GO_BACKWARD_DISTANCE]: {
-		definition: {
-			init: function(this: Blockly.Block): void {
-				this.appendDummyInput()
-					.appendField("Go backward")
-
-				// Create the inches label field first so we can reference it
-				const inchesLabelField = new Blockly.FieldLabelSerializable("inches")
-
-				// Add distance input
-				const distanceField = new Blockly.FieldNumber(10, 1, 240, 0.05)
-				this.appendDummyInput()
-					.appendField(distanceField, MOTOR_FIELD_VALUES.DRIVING_DISTANCE)
-					.appendField(inchesLabelField, "INCHES_LABEL")
-					.appendField("at")
-
-				// Add speed input
-				const percentField = new Blockly.FieldNumber(20, 0, 100, 1)
-				this.appendDummyInput()
-					.appendField(percentField, MOTOR_FIELD_VALUES.DRIVING_PERCENTAGE)
-					.appendField("% speed")
-
-				this.setPreviousStatement(true, null)
-				this.setNextStatement(true, null)
-				this.setColour(motorsCategoryColour)
-				this.setTooltip("Move Pip  backward for specified distance at given speed")
-				this.setInputsInline(true)
-
-				// Function to update the inches label
-				const updateInchesLabel = (newValue: number | string): number | string => {
-					const label = newValue === 1 ? "inch" : "inches"
-					inchesLabelField.setValue(label)
-					return newValue // Return the value unchanged
-				}
-
-				// Set validator on the number field
-				distanceField.setValidator(updateInchesLabel)
-
-				// Set initial label
-				updateInchesLabel(10)
-			},
-			keywords: ["motor", "reverse", "back", "retreat", "distance", "in", "inches", "length", "travel"]
-		},
-		generator: (block: Blockly.Block): string => {
-			const distance = block.getFieldValue(MOTOR_FIELD_VALUES.DRIVING_DISTANCE) || "0"
-			const percentage = block.getFieldValue(MOTOR_FIELD_VALUES.DRIVING_PERCENTAGE) || "0"
-			return `goBackwardDistance(${distance}, ${percentage});\n`
+			return `drive_distance(${toUpper(direction)}, ${distance}, ${percentage});\n`
 		}
 	},
 	[MOTOR_BLOCK_TYPES.TURN]: {
