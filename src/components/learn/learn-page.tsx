a6a0942a62fb074cef9f5c85e0457c99eeb31fbc
lever-labs <hello@leverlabs.com>
2025-10-21 17:31:07 -0400

    Update learn-page.tsx

--- src/components/learn/learn-page.tsx
+++ src/components/learn/learn-page.tsx
@@ -60,7 +60,7 @@ function LearnPage({ lessonId }: { lessonId: LessonUUID }): React.ReactNode {
 	return (
 		<div className="h-screen flex flex-col px-4 sm:px-6 md:px-8 lg:px-12 xl:px-60 2xl:px-96">
 			<LessonHeader lessonId={lessonId} />
-			<main className="flex-1 overflow-auto py-6 flex items-center justify-center">
+			<main className="flex-1 overflow-auto flex items-center justify-center">
 				<LessonQuestions lessonId={lessonId} />
 			</main>
 			<LessonFooter lessonId={lessonId} />
