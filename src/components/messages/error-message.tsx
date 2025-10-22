2e3199770f4fcb3a3f42db3911e384cb3d9d811a
lever-labs <hello@leverlabs.com>
2025-10-22 15:02:14 -0400

    Alert update

--- src/components/messages/error-message.tsx
+++ src/components/messages/error-message.tsx
@@ -11,13 +11,14 @@ export default function ErrorMessage(props: Props): React.ReactNode {
 	const { error } = props
 
 	return (
-		<Alert variant="destructive" className="dark:text-red-400 border-0">
-			<div className="flex items-center justify-center gap-2">
-				<AlertCircle className="h-6 w-6 shrink-0" />
-				<AlertDescription className="flex-1 text-center justify-center text-lg">
-					{error}
-				</AlertDescription>
-			</div>
+		<Alert
+			variant="destructive"
+			className="dark:text-red-400 border-0 justify-items-center text-center"
+		>
+			<AlertCircle className="h-6 w-6" />
+			<AlertDescription className="text-lg">
+				{error}
+			</AlertDescription>
 		</Alert>
 	)
 }
