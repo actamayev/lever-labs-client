b354d10d7ec6e1de1b6f79e20fe476e79fe9826e
lever-labs <hello@leverlabs.com>
2025-10-23 10:52:33 -0400

    Update skeleton.tsx

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,17 +1,13 @@
-"use client"
-
 import { cn } from "@/lib/shadcn/utils"
 
-function Skeleton({
-	className,
-	...props
-}: React.HTMLAttributes<HTMLDivElement>) {
-	return (
-		<div
-			className={cn("animate-pulse rounded-md bg-primary/10", className)}
-			{...props}
-		/>
-	)
+function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="skeleton"
+      className={cn("bg-accent animate-pulse rounded-md", className)}
+      {...props}
+    />
+  )
 }
 
 export { Skeleton }
