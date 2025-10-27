2348efb79e200140b037fb109a73fbc55d9d7d69
lever-labs <hello@leverlabs.com>
2025-10-27 14:21:22 -0400

    Update hero-background.tsx

--- src/components/landing/hero-background.tsx
+++ src/components/landing/hero-background.tsx
@@ -1,28 +1,36 @@
+import Image from "next/image"
+
 export default function HeroBackground(): React.ReactNode {
 	return (
 		<>
 			{/* Desktop/tablet image */}
-			<img
+			<Image
 				className="wide:opacity-0 sm:opacity-100 opacity-0 w-full h-full object-cover absolute"
 				src="/trmnl-hero.jpg"
 				alt="TRMNL hero"
 				style={{ objectPosition: "left center" }}
+				fill
+				priority
 			/>
 
 			{/* Wide screen image */}
-			<img
+			<Image
 				className="wide:opacity-100 opacity-0 w-full h-full object-cover absolute"
 				src="/trmnl-hero-wide.jpg"
 				alt="TRMNL hero wide"
 				style={{ objectPosition: "center center" }}
+				fill
+				priority
 			/>
 
 			{/* Mobile image */}
-			<img
+			<Image
 				className="sm:opacity-0 opacity-100 w-full h-full object-cover absolute"
 				src="/trmnl-hero-mobile.jpg"
 				alt="TRMNL hero mobile"
 				style={{ objectPosition: "center center" }}
+				fill
+				priority
 			/>
 		</>
 	)
