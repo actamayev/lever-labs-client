24c5da9ea0db297d16dce3414b375df2a16607d9
lever-labs <hello@leverlabs.com>
2025-10-28 11:20:36 -0400

    Fix logout issue

--- src/components/auth/login/login-component.tsx
+++ src/components/auth/login/login-component.tsx
@@ -122,7 +122,7 @@ function LoginComponent(): React.ReactNode {
 				<Image
 					src="/favicon.svg"
 					alt="Image"
-					className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+					className="absolute inset-0 h-full w-full object-cover"
 					fill
 					priority
 				/>
--- src/components/auth/register/register-component.tsx
+++ src/components/auth/register/register-component.tsx
@@ -134,7 +134,7 @@ function RegisterComponent(): React.ReactNode {
 				<Image
 					src="/favicon.svg"
 					alt="Image"
-					className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+					className="absolute inset-0 h-full w-full object-cover"
 					fill
 					priority
 				/>
