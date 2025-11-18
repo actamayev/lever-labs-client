8b9b84f6da2d34db554fca17215728ed081f9a93
lever-labs <hello@leverlabs.com>
2025-11-18 12:46:57 -0500

    Update demos-section.tsx

--- src/components/garage/tones-and-demos/demos-section.tsx
+++ src/components/garage/tones-and-demos/demos-section.tsx
@@ -2,12 +2,12 @@
 
 import { observer } from "mobx-react"
 import { BlueTactileButton } from "../../buttons/tactile-buttons"
-// import garageClass from "../../../classes/garage-class"
+import garageClass from "../../../classes/garage-class"
 
 function DemosSection(): React.ReactNode {
-	// const handleSensorDataClick = (): void => {
-	// garageClass.setIsSensorDataOpen(!garageClass.isSensorDataOpen)
-	// }
+	const handleSensorDataClick = (): void => {
+		garageClass.setIsSensorDataOpen(!garageClass.isSensorDataOpen)
+	}
 
 	return (
 		<div className="h-full flex flex-col items-center justify-between w-full my-10">
@@ -16,7 +16,7 @@ function DemosSection(): React.ReactNode {
 			{/* <BlueTactileButton className="w-[80%] h-10 text-2xl duration-0">
 				DEMO LIBRARY
 			</BlueTactileButton> */}
-			<BlueTactileButton className="w-[80%] h-10 text-2xl duration-0">
+			<BlueTactileButton className="w-[80%] h-10 text-2xl duration-0" onClick={handleSensorDataClick}>
 				SENSOR DATA
 			</BlueTactileButton>
 			<div className="w-[80%] h-10 text-2xl duration-0"/>
